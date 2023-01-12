fb1ff4ae9469857ff62ef9708adc8c6317f5b2e6
Anders Mogensen <am@mapspeople.com>
2023-01-12 10:52:35 +0100

    feat: Implement basic component structure. The React app will have a MapsIndoors map component, which contains a (non-working) map and should contain other top-level components for the Map Template.

--- packages/mapsindoors-map-react/src/App.js
+++ packages/mapsindoors-map-react/src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
+import MapsIndoorsMap from './components/MapsIndoorsMap/MapsIndoorsMap';
 
 function App() {
     return (
         <div className="app">
             <h1>MapsIndoors Map</h1>
             <mi-icon icon-name="marker"></mi-icon>
+            <div className="app__map-container">
+
+
+                {/* This is the Map Template component */}
+                <MapsIndoorsMap
+                    apiKey="mapspeople"
+                    mapboxAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
+                />
+
+
+            </div>
         </div>
     );
 }
