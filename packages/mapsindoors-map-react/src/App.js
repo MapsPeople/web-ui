82b7869127805f7af6090e960d2e7c9022b87740
Andreea Ceachir <ance@mapspeople.com>
2023-01-16 12:55:01 +0100

    refactor: Illustrate the splash screen inside an existing app

--- packages/mapsindoors-map-react/src/App.js
+++ packages/mapsindoors-map-react/src/App.js
@@ -4,13 +4,15 @@ import MapsIndoorsMap from './components/MapsIndoorsMap/MapsIndoorsMap';
 function App() {
     return (
         <div className="app">
+            <h1>MapsIndoors Map</h1>
+            <mi-icon icon-name="marker"></mi-icon>
             <div className="app__map-container">
                 {/* This is the Map Template component */}
                 <MapsIndoorsMap
                     apiKey="mapspeople"
                     venue="INDUSTRIAL_TERRACE"
                     mapboxAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
-                    // gmApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}
+                // gmApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}
                 />
             </div>
         </div>
