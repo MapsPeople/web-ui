c5fa612514a1dca6d5e6015456553ea9447ca23e
Anders Mogensen <am@mapspeople.com>
2023-01-16 10:59:35 +0100

    feat: Render a list of Venues using the Venue component.

--- packages/mapsindoors-map-react/src/components/VenueSelector/VenueSelector.jsx
+++ packages/mapsindoors-map-react/src/components/VenueSelector/VenueSelector.jsx
@@ -1,18 +1,29 @@
 import { useState } from 'react';
 import './VenueSelector.scss';
 import { ReactComponent as BuildingLogo } from './../../assets/building.svg';
+import Venue from './Venue/Venue';
 
-function VenueSelector() {
+/**
+ *
+ * @param {object} props
+ * @param {array} props.venues
+ * @returns
+ */
+function VenueSelector({ venues }) {
     const [active, setActive] = useState(false);
 
-    return <div className="venue-selector">
+    const setVenue = () => {
+        // TODO: Set the venue
+    }
+
+    return <>
         <button className="venue-selector__button" onClick={() => setActive(current => !current)} aria-label="Venues">
             <BuildingLogo />
         </button>
         {active && <div className="venue-selector__list">
-            Venues list...
+            {venues.map(venue => (<Venue key={venue.id} venue={venue} onVenueSelected={() => setVenue(venue)} />))}
         </div>}
-    </div>
+    </>
 }
 
 export default VenueSelector;
