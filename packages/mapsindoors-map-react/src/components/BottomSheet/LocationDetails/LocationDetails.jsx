537c589b3ec14c3f36e4b53345eee3b7873bf3ee
Anders Mogensen <am@mapspeople.com>
2023-01-25 11:24:14 +0100

    refactor: avoid using the mi-list-item-location component as it does not fit our needs.

--- packages/mapsindoors-map-react/src/components/BottomSheet/LocationDetails/LocationDetails.jsx
+++ packages/mapsindoors-map-react/src/components/BottomSheet/LocationDetails/LocationDetails.jsx
@@ -12,8 +12,16 @@ function LocationDetails({ location }) {
     }, [location]);
 
     return <div className="location-details">
-        <mi-list-item-location ref={locationInfoElement} />
-        {location.properties.imageURL && <img alt="" src={location.properties.imageURL} className="location-details__image" />}
+            <div className="location-details__info location-info">
+                <div className="location-info__icon">
+                    [i] {/* FIXME: Location icon from Display Rule */}
+                </div>
+                <div className="location-info__content">
+                    {location?.properties.name}<br />
+                    <mi-location-info ref={locationInfoElement} />
+                </div>
+            </div>
+            {location.properties.imageURL && <img alt="" src={location.properties.imageURL} className="location-details__image" />}
     </div>
 }
 
