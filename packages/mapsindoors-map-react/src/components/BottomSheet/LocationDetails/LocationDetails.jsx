391ea53d2f51a84752418e283a1c29e0aec73eef
Anders Mogensen <am@mapspeople.com>
2023-01-30 12:39:36 +0100

    chore: cleanup.

--- packages/mapsindoors-map-react/src/components/BottomSheet/LocationDetails/LocationDetails.jsx
+++ packages/mapsindoors-map-react/src/components/BottomSheet/LocationDetails/LocationDetails.jsx
@@ -28,41 +28,41 @@ function LocationDetails({ location, onClose }) {
     }, [location, mapsIndoorsInstance]);
 
     return <div className="location-details">
-            {location && <>
-                <div className="location-info">
-                    <div className="location-info__icon">
-                        {locationDisplayRyle && <img alt="" src={locationDisplayRyle.icon} />}
-                    </div>
-                    <div className="location-info__content">
-                        {location.properties.name}<br />
-                        <mi-location-info ref={locationInfoElement} />
-                    </div>
-                    <button className="location-info__close" onClick={() => onClose()}>
-                        <CloseIcon />
-                    </button>
+        {location && <>
+            <div className="location-info">
+                <div className="location-info__icon">
+                    {locationDisplayRyle && <img alt="" src={locationDisplayRyle.icon} />}
                 </div>
-
-                {location.properties.imageURL && <img alt="" src={location.properties.imageURL} className="location-details__image" />}
-
-                {Object.keys(location.properties.categories).length > 0 && <p className="location-details__categories">
-                    {Object.values(location.properties.categories).map((category, index, array) => {
-                        return <React.Fragment key={category}>{category}{index < array.length-1 && <>・</>}</React.Fragment>
-                    })}
-                </p>}
-
-                {location.properties.description && <section className={`location-details__description ${showFullDescription ? 'location-details__description--full' : ''}`}>
-                    <h4>Description</h4>
-                    <div ref={locationDetailsElement}>
-                        {location.properties.description}
-                    </div>
-                    {(isOverflowing || showFullDescription) && <button onClick={() => setShowFullDescription(!showFullDescription)}>{!showFullDescription ? 'Read full description' : 'Close' }</button>}
-                </section>}
-                <button className="location-details__wayfinding">
-                    <PinIcon />
-                    Start wayfinding
-                    {/* FIXME: Implement */}
+                <div className="location-info__content">
+                    {location.properties.name}<br />
+                    <mi-location-info ref={locationInfoElement} />
+                </div>
+                <button className="location-info__close" onClick={() => onClose()}>
+                    <CloseIcon />
                 </button>
-            </>}
+            </div>
+
+            {location.properties.imageURL && <img alt="" src={location.properties.imageURL} className="location-details__image" />}
+
+            {Object.keys(location.properties.categories).length > 0 && <p className="location-details__categories">
+                {Object.values(location.properties.categories).map((category, index, array) => {
+                    return <React.Fragment key={category}>{category}{index < array.length-1 && <>・</>}</React.Fragment>
+                })}
+            </p>}
+
+            {location.properties.description && <section className={`location-details__description ${showFullDescription ? 'location-details__description--full' : ''}`}>
+                <h4>Description</h4>
+                <div ref={locationDetailsElement}>
+                    {location.properties.description}
+                </div>
+                {(isOverflowing || showFullDescription) && <button onClick={() => setShowFullDescription(!showFullDescription)}>{!showFullDescription ? 'Read full description' : 'Close' }</button>}
+            </section>}
+            <button className="location-details__wayfinding">
+                <PinIcon />
+                Start wayfinding
+                {/* FIXME: Implement */}
+            </button>
+        </>}
     </div>
 }
 
