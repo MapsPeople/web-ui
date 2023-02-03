79ce1780345421e5b2cb33d1dbd8857c082068f6
Mark Hougaard <mark@hougaard.email>
2023-02-03 22:06:01 +0100

    fix: Rename package to components

--- packages/mapsindoors-map-react/src/index.js
+++ packages/mapsindoors-map-react/src/index.js
@@ -1,9 +1,9 @@
+import { defineCustomElements } from '@mapsindoors/components';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
+import './index.css';
 import reportWebVitals from './reportWebVitals';
-import { defineCustomElements } from 'mi-components/dist/esm/loader.js';
 
 defineCustomElements();
 const root = ReactDOM.createRoot(document.getElementById('root'));
