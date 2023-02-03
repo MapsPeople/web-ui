1ac67945d58907b8d68ef3bc33c22f0a34c38035
Mark Hougaard <mark@hougaard.email>
2023-02-03 16:40:08 +0100

    chore: Update with latest changes from GitLab

--- packages/mi-components/src/components/keyboard/keyboard-alphabetic-layouts.ts
+++ packages/mi-components/src/components/keyboard/keyboard-alphabetic-layouts.ts
@@ -17,5 +17,24 @@ export const danishAlphabetic = {
         '{space}'
     ]
 };
+export const germanAlphabetic = {
+    default: [
+        '1 2 3 4 5 6 7 8 9 0',
+        'q w e r t z u i o p ü',
+        'a s d f g h j k l ö ä',
+        'y x c v b n m {bksp}',
+        '{space}'
+    ]
+};
+
+export const frenchAlphabetic = {
+    default: [
+        '1 2 3 4 5 6 7 8 9 0',
+        'a z e r t y u i o p',
+        'q s d f g h j k l m',
+        'w x c v b n {bksp}',
+        '{space}'
+    ]
+};
 
 export const defaultAlphabetic = unitedStatesAlphabetic;
