5422ecede40c2efba197fa04c8393616543b96a6
Anders Mogensen <am@mapspeople.com>
2023-02-06 12:19:57 +0100

    feat: It is now possible to drag the sheet to any size.

--- packages/mapsindoors-map-react/src/components/BottomSheet/Sheet/Sheet.jsx
+++ packages/mapsindoors-map-react/src/components/BottomSheet/Sheet/Sheet.jsx
@@ -1,10 +1,55 @@
+import { useEffect } from 'react';
+import { useRef, useState } from 'react';
 import './Sheet.scss';
 
 function Sheet({ children, isOpen }) {
 
-    return <div className={`sheet ${isOpen ? 'sheet--active' : ''}`}>
-        <div className="sheet__drag">
-            {/* FIXME: Make sheet draggable when dragging this div */}
+    const draggerRef = useRef();
+
+    const [style, setStyle] = useState({});
+    const [isDragging, setIsDragging] = useState(false);
+
+    /*
+     * Reset hardcoded height when children or isOpen change.
+     */
+    useEffect(() => {
+        setStyle({});
+    }, [children, isOpen]);
+
+    /*
+     * Dragging functionality: Make it possible to drag the sheet to another size.
+     */
+    function startDragging(event) {
+        setIsDragging(true);
+
+        // While dragging, prevent default touch move since it will otherwise take over the interaction.
+        const preventTouchMoveHandler = event => {
+            event.preventDefault();
+        }
+
+        const mouseMoveHandler = event => {
+            const rect = draggerRef.current.parentNode.getBoundingClientRect();
+            const bottomOffset = window.innerHeight - rect.bottom;
+            const newHeight = window.innerHeight - event.clientY - bottomOffset;
+
+            if (newHeight >= 0) { // FIXME: also restrict to a max height
+                setStyle({ height: `${newHeight}px`});
+            }
+        };
+
+        document.addEventListener('touchmove', preventTouchMoveHandler, { passive: false });
+        document.addEventListener('pointermove', mouseMoveHandler);
+
+        window.addEventListener('pointerup', function mouseUpHandler() {
+            document.removeEventListener('pointermove', mouseMoveHandler);
+            window.removeEventListener('pointerup', mouseUpHandler);
+            document.removeEventListener('touchmove', preventTouchMoveHandler);
+            setIsDragging(false);
+        });
+    }
+
+    return <div style={style} className={`sheet ${isOpen ? 'sheet--active' : ''} ${isDragging ? 'sheet--dragging' : ''}`}>
+        <div onPointerDown={(e) => startDragging(e)} ref={draggerRef} className="sheet__drag">
             <div className="sheet__drag-icon"></div>
         </div>
         {children}
