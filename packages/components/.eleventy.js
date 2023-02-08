c8eec0711cd61f5910b051228ff61b3119edc06a
Mark Hougaard <mark@hougaard.email>
2023-02-08 23:02:45 +0100

    chore: Fix doc urls

--- packages/components/.eleventy.js
+++ packages/components/.eleventy.js
@@ -1,4 +1,7 @@
+const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
+
 module.exports = function (eleventyConfig) {
+    eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
     eleventyConfig.addPassthroughCopy({ "www/build": "_js" });
     eleventyConfig.addPassthroughCopy({ "src/docs/assets": "/" });
 
@@ -21,6 +24,7 @@ module.exports = function (eleventyConfig) {
         port: 8000,
         ui: false,
     });
+
 
     return {
         dir: {
