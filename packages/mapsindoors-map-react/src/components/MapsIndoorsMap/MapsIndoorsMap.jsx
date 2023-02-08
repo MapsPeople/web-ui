651ced1fc578908329d0cd9e6be6077d3d7a291a
Andreea Ceachir <ance@mapspeople.com>
2023-02-08 11:06:52 +0100

    chore: Remove test data for faking delay when data is loading.

--- packages/mapsindoors-map-react/src/components/MapsIndoorsMap/MapsIndoorsMap.jsx
+++ packages/mapsindoors-map-react/src/components/MapsIndoorsMap/MapsIndoorsMap.jsx
@@ -61,17 +61,10 @@ function MapsIndoorsMap({ apiKey, gmApiKey, mapboxAccessToken, venue, locationId
 
         // Fetch venue information when we know data is loaded and set the map to be in a ready state.
         mapsindoors.services.LocationsService.once('update_completed', () => {
-
-            const fakePromise = new Promise((resolve) => {
-                setTimeout(() => {
-                    resolve();
-                }, 6000);
-            })
-
             // Fixme: Venue Images are currently stored in the AppConfig object. So we will need to read the AppConfig as well as the list of Venues.
             // This will be changed in the future.
 
-            Promise.all([mapsindoors.services.VenuesService.getVenues(), mapsindoors.services.AppConfigService.getConfig(), fakePromise]).then(([venuesResult, appConfigResult, _]) => {
+            Promise.all([mapsindoors.services.VenuesService.getVenues(), mapsindoors.services.AppConfigService.getConfig()]).then(([venuesResult, appConfigResult, _]) => {
                 venuesResult = venuesResult.map(venue => {
                     venue.image = appConfigResult.venueImages[venue.name.toLowerCase()];
                     return venue;
@@ -81,10 +74,6 @@ function MapsIndoorsMap({ apiKey, gmApiKey, mapboxAccessToken, venue, locationId
 
                 timeAfterDataIsLoaded = new Date();
                 timeToLoadData = (timeAfterDataIsLoaded - startTime) / 1000;
-
-                console.log('current time', startTime)
-                console.log('time after data is loaded', timeAfterDataIsLoaded)
-                console.log('time to load data', timeToLoadData);
 
                 if (timeToLoadData >= 3) {
                     setMapReady(true);
@@ -99,7 +88,7 @@ function MapsIndoorsMap({ apiKey, gmApiKey, mapboxAccessToken, venue, locationId
 
     return (<MapsIndoorsContext.Provider value={mapsIndoorsInstance}>
         <div className="mapsindoors-map">
-            {!isMapReady && <SplashScreen logo={logo} primaryColor={primaryColor} className={isMapReady ? 'ready' : ''} />}
+            {!isMapReady && <SplashScreen logo={logo} primaryColor={primaryColor} />}
             {venues.length > 1 && <VenueSelector onVenueSelected={selectedVenue => setCurrentVenueName(selectedVenue.name)} venues={venues} currentVenueName={currentVenueName} />}
             {isMapReady && <BottomSheet currentLocation={currentLocation} onClose={() => setCurrentLocation(null)} />}
             <Map apiKey={apiKey} gmApiKey={gmApiKey} mapboxAccessToken={mapboxAccessToken} venues={venues} venueName={currentVenueName} onMapsIndoorsInstance={(instance) => setMapsIndoorsInstance(instance)} onLocationClick={(location) => setCurrentLocation(location)} />
