095b0b8c7077f8fd36e9cf7de3ee8144242cf625
Andreea Ceachir <ance@mapspeople.com>
2023-02-09 16:17:34 +0100

    chore: Add helper comments.

--- packages/mapsindoors-map-react/src/components/MapsIndoorsMap/MapsIndoorsMap.jsx
+++ packages/mapsindoors-map-react/src/components/MapsIndoorsMap/MapsIndoorsMap.jsx
@@ -56,31 +56,35 @@ function MapsIndoorsMap({ apiKey, gmApiKey, mapboxAccessToken, venue, locationId
      * Set the map to be in a ready state when the data has loaded.
      */
     useEffect(() => {
+        // Keep track of the time when the process starts and the time when the data has been loaded. 
+        // Calculate the time taken for the data to load, and determine the time that the splash screen should be shown.
+        // The splash screen should be shown for minimum 3 seconds, and if the data takes longer to load, the splash screen should not disappear.
         const startTime = new Date();
         setMapReady(false);
         mapsindoors.MapsIndoors.setMapsIndoorsApiKey(apiKey);
 
         // Fetch venue information when we know data is loaded and set the map to be in a ready state.
         mapsindoors.services.LocationsService.once('update_completed', () => {
+
             // Fixme: Venue Images are currently stored in the AppConfig object. So we will need to read the AppConfig as well as the list of Venues.
             // This will be changed in the future.
-
             Promise.all([mapsindoors.services.VenuesService.getVenues(), mapsindoors.services.AppConfigService.getConfig()]).then(([venuesResult, appConfigResult]) => {
                 venuesResult = venuesResult.map(venue => {
                     venue.image = appConfigResult.venueImages[venue.name.toLowerCase()];
                     return venue;
                 });
-
                 setVenues(venuesResult);
-
+                
+                // Determine the time when the data has finished loading. 
                 const timeAfterDataIsLoaded = new Date();
+
                 // Divide the number by 1000 in order to get the value in seconds.
                 const timeToLoadData = (timeAfterDataIsLoaded - startTime) / 1000;
 
                 // Subtract the time that took to load the data in order to get the differece. 
-                // The minimum loading time for the splash screen is 3 seconds.
                 const timeDifference = (3 - timeToLoadData) * 1000
 
+                // Determine whether to set the map ready or not based on the loading time. 
                 if (timeToLoadData >= 3) {
                     setMapReady(true);
                 } else {
