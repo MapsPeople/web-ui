ada2d267a25c69a6aef198e4590fd0a38f3171d6
Andreea Ceachir <ance@mapspeople.com>
2023-02-16 11:23:02 +0100

    chore: Add initial implementation of search results.

--- packages/mapsindoors-map-react/src/components/Wayfinding/Wayfinding.jsx
+++ packages/mapsindoors-map-react/src/components/Wayfinding/Wayfinding.jsx
@@ -7,6 +7,43 @@ import { ReactComponent as WalkingIcon } from '../../assets/walking.svg';
 import { ReactComponent as QuestionIcon } from '../../assets/question.svg';
 
 function Wayfinding({ onStartDirections, onBack }) {
+    const toSearch = document.getElementById('to');
+    const fromSearch = document.getElementById('from');
+    const details = document.getElementById('details')
+
+    const resultsContainer = document.getElementById('results');
+
+    if (fromSearch && toSearch) {
+        fromSearch.addEventListener('results', e => {
+            resultsContainer.innerHTML = '';
+            for (const result of e.detail) {
+                const listItem = document.createElement('mi-list-item-location');
+                listItem.location = result;
+                resultsContainer.appendChild(listItem);
+            }
+        });
+        fromSearch.addEventListener('cleared', () => {
+            resultsContainer.innerHTML = '';
+        });
+
+        toSearch.addEventListener('results', e => {
+            resultsContainer.innerHTML = '';
+            for (const result of e.detail) {
+                const listItem = document.createElement('mi-list-item-location');
+                listItem.location = result;
+                resultsContainer.appendChild(listItem);
+                listItem.addEventListener('locationClicked', console.log('clicked'))
+            }
+        });
+        toSearch.addEventListener('cleared', () => {
+            resultsContainer.innerHTML = '';
+        })
+
+        toSearch.addEventListener('click', () => {
+            details.classList.add('hide');
+        });
+    }
+
     return (
         <div className="wayfinding">
             <div className="wayfinding__directions">
@@ -19,18 +56,21 @@ function Wayfinding({ onStartDirections, onBack }) {
                         <div className="wayfinding__label">
                             TO
                         </div>
-                        <mi-search placeholder="Search by name, category, building..." id="first"></mi-search>
+                        <mi-search placeholder="Search by name, category, building..." id="to" mapsindoors="true"></mi-search>
                     </div>
                     <div className="wayfinding__from">
                         <div className="wayfinding__label">
                             FROM
                         </div>
-                        <mi-search placeholder="Search by name, category, building..." id="second"></mi-search>
+                        <mi-search placeholder="Search by name, category, building..." id="from" mapsindoors="true"></mi-search>
                     </div>
                 </div>
             </div>
+            <div className="wayfinding__results" id="results">
 
-            <div className="wayfinding__details">
+            </div>
+
+            <div className="wayfinding__details" id="details">
                 <div className="wayfinding__accessibility">
                     <input className="mi-toggle" type="checkbox" />
                     <div>Accessibility</div>
