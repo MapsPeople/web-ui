2bc3146a8ce44906531100b78059ef5d569a3491
Anders Mogensen <am@mapspeople.com>
2023-03-01 11:00:14 +0100

    feat: Add MapsIndoors Floor Selector to the maps.

--- packages/mapsindoors-map-react/src/components/Map/GoogleMapsMap/GoogleMapsMap.jsx
+++ packages/mapsindoors-map-react/src/components/Map/GoogleMapsMap/GoogleMapsMap.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { Loader as GoogleMapsApiLoader } from '@googlemaps/js-api-loader';
 
 // Make the global MapsIndoors JavaScript SDK available here
@@ -10,17 +10,24 @@ const mapsindoors = window.mapsindoors;
  * @param {object} props
  * @param {string} props.gmApiKey - A Google Maps JS API key required for loading the Google Maps JS API.
  * @param {function} props.onMapView - A function that is called when the MapView is constructed.
+ * @param {object} props.mapsIndoorsInstance - Instance of the mapsindoors.MapsIndoors
  */
-function GoogleMapsMap({ gmApiKey, onMapView}) {
-    useEffect(() => {
+function GoogleMapsMap({ gmApiKey, onMapView, mapsIndoorsInstance }) {
 
+    const [google, setGoogle] = useState();
+    const [mapView, setMapView] = useState();
+    const [hasFloorSelector, setHasFloorSelector] = useState(false);
+
+    useEffect(() => {
         const loader = new GoogleMapsApiLoader({
             apiKey: gmApiKey,
             version: 'quarterly',
             libraries: ['geometry']
         });
 
-        loader.load().then(google => {
+        loader.load().then(loadedGoogle => {
+            setGoogle(loadedGoogle);
+
             // Initialize Google Maps MapView
             const mapViewOptions = {
                 element: document.getElementById('map'),
@@ -28,10 +35,21 @@ function GoogleMapsMap({ gmApiKey, onMapView}) {
             };
 
             const mapViewInstance = new mapsindoors.mapView.GoogleMapsView(mapViewOptions);
-            onMapView(mapViewInstance)
+            setMapView(mapViewInstance);
+            onMapView(mapViewInstance);
         });
     }, []); // eslint-disable-line react-hooks/exhaustive-deps
     // We ignore eslint warnings about missing dependencies because onMapView should never change runtime and changing Google Maps API key runtime will give other problems.
+
+    // Add Floor Selector to the Map when ready.
+    useEffect(() => {
+        if (mapsIndoorsInstance && mapView && google && !hasFloorSelector) {
+            const floorSelectorDiv = document.createElement('div');
+            new mapsindoors.FloorSelector(floorSelectorDiv, mapsIndoorsInstance);
+            mapView.getMap().controls[google.maps.ControlPosition.RIGHT_TOP].push(floorSelectorDiv);
+            setHasFloorSelector(true);
+        }
+    }, [mapsIndoorsInstance, mapView, google, hasFloorSelector])
 
     return <div className="map-container" id="map"></div>
 }
--- packages/mapsindoors-map-react/src/components/Map/MapboxMap/MapboxMap.jsx
+++ packages/mapsindoors-map-react/src/components/Map/MapboxMap/MapboxMap.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import mapboxgl from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 
@@ -11,8 +11,12 @@ const mapsindoors = window.mapsindoors;
  * @param {object} props
  * @param {string} props.mapboxAccessToken - A Mapbox Access Token required for showing the map.
  * @param {function} props.onMapView - A function that is called when the MapView is constructed.
+ * @param {object} props.mapsIndoorsInstance - Instance of the mapsindoors.MapsIndoors
  */
-function MapboxMap({ mapboxAccessToken, onMapView }) {
+function MapboxMap({ mapboxAccessToken, onMapView, mapsIndoorsInstance }) {
+
+    const [mapView, setMapView] = useState();
+    const [hasFloorSelector, setHasFloorSelector] = useState(false);
 
     useEffect(() => {
         // Initialize MapboxView MapView
@@ -23,11 +27,28 @@ function MapboxMap({ mapboxAccessToken, onMapView }) {
         };
 
         const mapViewInstance = new mapsindoors.mapView.MapboxView(mapViewOptions);
+        setMapView(mapViewInstance);
         onMapView(mapViewInstance);
     }, []); // eslint-disable-line react-hooks/exhaustive-deps
     // We ignore eslint warnings about missing dependencies because onMapView should never change runtime and changing Mapbox Access Token runtime will give other problems.
 
+    // Add Floor Selector to the Map when ready.
+    useEffect(() => {
+        if (mapsIndoorsInstance && mapView && !hasFloorSelector) {
+            const floorSelectorDiv = document.createElement('div');
+            new mapsindoors.FloorSelector(floorSelectorDiv, mapsIndoorsInstance);
+            mapView.getMap().addControl({
+                onAdd: () => floorSelectorDiv,
+                onRemove: () => {
+                    floorSelectorDiv.parentNode.removeChild(floorSelectorDiv);
+                }
+            }, 'top-right');
+            setHasFloorSelector(true);
+        }
+    }, [mapsIndoorsInstance, mapView, hasFloorSelector]);
+
     return <div className="map-container" id="map"></div>
 }
+
 
 export default MapboxMap;
