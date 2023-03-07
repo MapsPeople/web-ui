157319c90910df9fa350ece3877308b4f82446d8
Andreea Ceachir <113503166+andreeaceachir142@users.noreply.github.com>
2023-03-07 13:39:12 +0100

    Update packages/components/src/components/search/search.tsx
    Co-authored-by: Anders Mogensen <am@mapspeople.com>

--- packages/components/src/components/search/search.tsx
+++ packages/components/src/components/search/search.tsx
@@ -164,8 +164,8 @@ export class Search implements ComponentInterface {
 
     /**
      * Set focus on the input field.
-     * Pass the preventScroll boolean as an option to determine whether the browser
-     * should scroll the document to bring the newly-focused element into view.
+     * The preventScroll boolean is passed as true to prevent the browser
+     * from scrolling the document to bring the newly-focused element into view.
      */
     @Method()
     focusInput(): void {
