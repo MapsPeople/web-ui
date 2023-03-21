a1a92b2bd8bdce4717ef4f9397a93b1ad3353f20
Andreea Ceachir <ance@mapspeople.com>
2023-03-21 13:47:17 +0100

    chore: Clear search results when clearing the field.

--- packages/mapsindoors-map-react/src/components/Wayfinding/Wayfinding.jsx
+++ packages/mapsindoors-map-react/src/components/Wayfinding/Wayfinding.jsx
@@ -152,6 +152,7 @@ function Wayfinding({ onStartDirections, onBack, location, onSetSize, isActive, 
             setFromFieldDisplayText('');
             setOriginLocation();
         }
+        setSearchResults([]);
     }
 
 
