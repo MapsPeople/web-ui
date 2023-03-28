34d4f447c77484c8efedf6de74021ee760929163
Mark Hougaard <mark@hougaard.email>
2023-03-28 21:50:25 +0200

    chore: Update app styles to mimick demo

--- packages/map-template/src/components/Map/Map.jsx
+++ packages/map-template/src/components/Map/Map.jsx
@@ -80,6 +80,9 @@ function Map({ gmApiKey, mapboxAccessToken, venues, venueName, onLocationClick, 
         const miInstance = new mapsindoors.MapsIndoors({
             mapView
         });
+        
+        // TODO: This overrides the pink building outline color from the SDK. It's added here for demo purposes until the SDK supports Display Rules for Buildings too.
+        miInstance.setDisplayRule('MI_BUILDING_OUTLINE', {visible: false});
 
         miInstance.on('click', location => onLocationClick(location));
 
