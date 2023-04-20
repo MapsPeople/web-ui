cce7fa25222ce5b28f1fe779c903e6d35bc99a17
Andreea Ceachir <ance@mapspeople.com>
2023-04-20 10:06:23 +0200

    chore: Add google default icon.

--- packages/components/src/components/list-item-location/list-item-location.tsx
+++ packages/components/src/components/list-item-location/list-item-location.tsx
@@ -80,8 +80,13 @@ export class ListItemLocation {
     }
 
     componentWillLoad(): void {
-        this.iconURLToRender = this.icon ? this.icon : this.location?.properties.imageURL;
-        this.updateBadge();
+        // Google default icon
+        if (this.location.properties.type === 'google_places') {
+            this.iconURLToRender = "data:image/svg+xml,%3Csvg width='10' height='10' viewBox='0 0 14 20' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M7 0C3.13 0 0 3.13 0 7C0 12.25 7 20 7 20C7 20 14 12.25 14 7C14 3.13 10.87 0 7 0ZM7 9.5C5.62 9.5 4.5 8.38 4.5 7C4.5 5.62 5.62 4.5 7 4.5C8.38 4.5 9.5 5.62 9.5 7C9.5 8.38 8.38 9.5 7 9.5Z' fill='black' fill-opacity='0.88'/%3E%3C/svg%3E%0A";
+        } else {
+            this.iconURLToRender = this.icon ? this.icon : this.location?.properties.imageURL;
+            this.updateBadge();
+        }
     }
 
     componentDidRender(): void {
