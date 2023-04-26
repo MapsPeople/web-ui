e6c84d9f52b685e176e4031dd6a7cd88b1701c30
Andreea Ceachir <ance@mapspeople.com>
2023-04-26 14:28:34 +0200

    chore: Remove testing data.

--- packages/map-template/src/App.js
+++ packages/map-template/src/App.js
@@ -8,8 +8,6 @@ function App() {
             <MapsIndoorsMap
                 apiKey="mapspeople"
                 venue="Stigsborgvej"
-                // tileStyle="https://tiles.mapsindoors.com/tiles/indoor/mi_packages_demo/advanced/100"
-                tileStyle=""
                 mapboxAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
                 // gmApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}
             />
