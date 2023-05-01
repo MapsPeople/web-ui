0112c62b59d5ac3c1c2cfc2d3911df19005dfc48
Andreea Ceachir <ance@mapspeople.com>
2023-05-01 14:48:04 +0200

    chore: Add support for showing the live data icon if present.

--- packages/map-template/src/components/WebComponentWrappers/ListItemLocation/ListItemLocation.jsx
+++ packages/map-template/src/components/WebComponentWrappers/ListItemLocation/ListItemLocation.jsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useRef, useState } from 'react';
+import { useContext, useEffect, useRef } from 'react';
 import { MapsIndoorsContext } from '../../../MapsIndoorsContext';
 
 /**
