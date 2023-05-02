7bb203f62fd1c62487dfbe3b004494a89bfcf380
Andreea Ceachir <ance@mapspeople.com>
2023-05-02 15:49:57 +0200

    chore: Test out query parameters on the mapsIndoorsMap component.

--- packages/map-template/src/App.js
+++ packages/map-template/src/App.js
@@ -2,14 +2,40 @@ import './App.css';
 import MapsIndoorsMap from './components/MapsIndoorsMap/MapsIndoorsMap';
 
 function App() {
+    const queryString = window.location.search;
+    const params = new URLSearchParams(queryString);
+
+    const apiKey = params.get('apiKey');
+    const venue = params.get('venue');
+    const locationId = params.get('locationId');
+    const primaryColor = params.get('primaryColor');
+    const hexPrimaryColor='#'.concat(primaryColor)
+    const logo = params.get('logo');
+    const appUserRoles = params.get('appUserRoles');
+
+    console.log('query string', queryString);
+    console.log('params', params);
+
+    console.log('apiKey', apiKey)
+    console.log('venue', venue)
+    console.log('locationId', locationId)
+    console.log('primaryColor', primaryColor)
+    console.log('hexPrimaryColor', hexPrimaryColor)
+    console.log('logo', logo)
+    console.log('appUserRoles', appUserRoles)
+
     return (
         <div className="app">
             {/* This is the Map Template component */}
             <MapsIndoorsMap
-                apiKey="mapspeople"
-                venue="Stigsborgvej"
+                apiKey={apiKey ? apiKey : 'demo'}
+                venue={venue}
+                locationId={locationId}
+                primaryColor={hexPrimaryColor ? hexPrimaryColor : undefined}
+                logo={logo ? logo : undefined}
+                appUserRoles={appUserRoles}
                 mapboxAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
-                // gmApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}
+            // gmApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}
             />
         </div>
     );
