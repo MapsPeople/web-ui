53e565c17c0a553c0da369170a489ee23aee363a
Andreea Ceachir <ance@mapspeople.com>
2023-05-03 14:35:52 +0200

    chore: Pass query parameters to the mapsIndoorsMap component.

--- packages/map-template/src/App.js
+++ packages/map-template/src/App.js
@@ -1,48 +1,41 @@
 import './App.css';
 import MapsIndoorsMap from './components/MapsIndoorsMap/MapsIndoorsMap';
 
+// The current query string
+const queryString = window.location.search;
+const params = new URLSearchParams(queryString);
+
+const apiKey = params.get('apiKey');
+const venue = params.get('venue');
+const locationId = params.get('locationId');
+const logo = params.get('logo');
+const mapboxAccessToken = params.get('mapboxAccessToken');
+const gmApiKey = params.get('gmApiKey');
+
+// Append the hashtag symbol to the colour code (i.e. ffffff)
+const primaryColor = '#'.concat(params.get('primaryColor'))
+
+// Create an array of elements based on the comma separated values
+const appUserRoles = params.get('appUserRoles')?.split(',')
+
+
 function App() {
-    const queryString = window.location.search;
-    const params = new URLSearchParams(queryString);
-
-    const apiKey = params.get('apiKey');
-    const venue = params.get('venue');
-    const locationId = params.get('locationId');
-    const primaryColor = params.get('primaryColor');
-    const hexPrimaryColor = '#'.concat(primaryColor)
-    const logo = params.get('logo');
-    const appUserRoles = params.get('appUserRoles');
-    const mapboxAccessToken = params.get('mapboxAccessToken');
-    const gmApiKey = params.get('gmApiKey');
-
-    console.log('query string', queryString);
-    console.log('params', params);
-
-    console.log('apiKey', apiKey)
-    console.log('venue', venue)
-    console.log('locationId', locationId)
-    console.log('primaryColor', primaryColor)
-    console.log('hexPrimaryColor', hexPrimaryColor)
-    console.log('logo', logo)
-    console.log('appUserRoles', appUserRoles)
-    console.log('mapboxAccessToken', mapboxAccessToken)
-    console.log('gmApiKey', gmApiKey)
-
     return (
         <div className="app">
             {/* This is the Map Template component */}
             <MapsIndoorsMap
                 apiKey={apiKey ? apiKey : 'demo'}
                 venue={venue}
-                locationId={locationId}
-                primaryColor={hexPrimaryColor ? hexPrimaryColor : undefined}
+                locationId={`${locationId}`}
+                primaryColor={primaryColor ? primaryColor : undefined}
                 logo={logo ? logo : undefined}
                 appUserRoles={appUserRoles}
                 mapboxAccessToken={mapboxAccessToken ? mapboxAccessToken : process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
-            // gmApiKey={gmApiKey ? gmApiKey : process.env.REACT_APP_GOOGLE_MAPS_API_KEY}
+                gmApiKey={gmApiKey ? gmApiKey : process.env.REACT_APP_GOOGLE_MAPS_API_KEY}
             />
         </div>
     );
 }
 
 export default App;
+
