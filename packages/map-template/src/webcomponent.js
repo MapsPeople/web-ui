82c70d9500ddc4a1ca0ebb2b4dd52f8f2b7518fe
Anders Mogensen <am@mapspeople.com>
2023-05-04 13:56:23 +0200

    build: Add the possibility of building a module that exports the MapsIndoorsMap component as a web component.

---
+++ packages/map-template/src/webcomponent.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import reactToWebComponent from 'react-to-webcomponent';
+import PropTypes from 'prop-types';
+import MapsIndoorsMap from './components/MapsIndoorsMap/MapsIndoorsMap';
+
+/*
+ * Exports the MapsIndoorsMap React component as a web component.
+ */
+
+MapsIndoorsMap.propTypes = {
+    apiKey: PropTypes.string.isRequired,
+    gmApiKey: PropTypes.string,
+    mapboxAccessToken: PropTypes.string,
+    venue: PropTypes.string,
+    locationId: PropTypes.string,
+    primaryColor: PropTypes.string,
+    logo: PropTypes.string,
+    appUserRoles: PropTypes.string
+}
+
+const WebMapsIndoorsMap = reactToWebComponent(MapsIndoorsMap, React, ReactDOM);
+
+export default WebMapsIndoorsMap;
