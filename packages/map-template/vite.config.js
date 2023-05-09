5f70d2b7943d01cd7d45b0edd0ed92156bae5ec9
Anders Mogensen <am@mapspeople.com>
2023-05-09 08:45:43 +0200

    build: change port when running in dev mode.

--- packages/map-template/vite.config.js
+++ packages/map-template/vite.config.js
@@ -5,6 +5,9 @@ import eslint from 'vite-plugin-eslint';
 
 export default defineConfig(() => {
     return {
+        server: {
+            port: 3000
+        },
         build: {
             outDir: 'build'
         },
