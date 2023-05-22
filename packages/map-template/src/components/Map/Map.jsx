811fc5566ea5ce3a87aa352b90ef3117bef3ac60
Andreea Ceachir <ance@mapspeople.com>
2023-05-22 13:53:51 +0200

    chore: Set the zoom level based on the solution's module.

--- packages/map-template/src/components/Map/Map.jsx
+++ packages/map-template/src/components/Map/Map.jsx
@@ -123,8 +123,16 @@ function Map({ apiKey, gmApiKey, mapboxAccessToken, venues, venueName, onLocatio
                     const locationGeometry = location.geometry.type === 'Point' ? location.geometry.coordinates : location.properties.anchor.coordinates;
                     miInstance.getMapView().setCenter({ lat: locationGeometry[1], lng: locationGeometry[0] });
 
-                    // Set the zoom level in order to see the location closer on the map.
-                    miInstance?.setZoom(21);
+                    // Check if the solution allows the zoom level to be 22.
+                    // If yes, set the zoom level to 22, otherwise set it to 21.
+                    mapsindoors.services.SolutionsService.getSolution().then(solution => {
+                        const hasZoom22 = Object.values(solution.modules).find(zoomLevel => zoomLevel === 'z22')
+                        if (hasZoom22) {
+                            miInstance?.setZoom(22);
+                        } else {
+                            miInstance?.setZoom(21);
+                        }
+                    });
                 }
             });
         }
