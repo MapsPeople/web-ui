cc14d7c9b05dfa13ed53a0ad55c046ca873e3d75
Anders Mogensen <am@mapspeople.com>
2023-05-25 12:15:02 +0200

    fix: allow mutability of mapsIndoorsInstance so it can save state internally.

--- packages/map-template/src/atoms/mapsIndoorsInstanceState.js
+++ packages/map-template/src/atoms/mapsIndoorsInstanceState.js
@@ -2,7 +2,8 @@ import { atom } from 'recoil';
 
 const mapsIndoorsInstanceState = atom({
     key: 'mapsIndoorsInstance',
-    default: null
+    default: null,
+    dangerouslyAllowMutability: true
 });
 
 export default mapsIndoorsInstanceState;
