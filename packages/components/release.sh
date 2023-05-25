558bafacdab0b543710a0d6138f493bfd1529d4a
Mark Hougaard <mark@hougaard.email>
2023-05-25 23:54:11 +0200

    build: Use shell script when triggering release

---
+++ packages/components/release.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+if [ "$1" != "major" ] && [ "$1" != "minor" ] && [ "$1" != "patch" ];
+then
+    echo "Could not release!"
+    echo "Usage: 'npm run release -- (major|minor|patch)'"
+    echo ""
+    exit 1
+fi
+
+npm version $1
+git add .
+export NPM_VERSION=$(npm pkg get version --workspaces=false | tr -d \")
+git commit -m 'Release Components'
+git tag components-v$NPM_VERSION
+echo "Bumped version to v$NPM_VERSION"
+git push && git push --tags
