9544d3147fdc82e9bc90f2079ab23bb7fd6d795a
Andreea Ceachir <ance@mapspeople.com>
2023-06-21 14:06:05 +0200

    chore: Refactor code.

--- packages/map-template/src/components/RouteInstructions/RouteInstructions.jsx
+++ packages/map-template/src/components/RouteInstructions/RouteInstructions.jsx
@@ -59,26 +59,28 @@ function RouteInstructions({ steps, onNextStep, onPreviousStep, originLocation }
             const lastStepCenter = mapsIndoorsInstance.getMapView().getCenter();
             setLastStepZoom(lastStepZoom);
             setLastStepCenter(lastStepCenter);
-        }
-        if (activeStep === totalSteps?.length - 1 && directions?.destinationLocation) {
-            // Get the destination location
-            const destinationLocation = directions?.destinationLocation;
 
-            // Center the map to the location coordinates.
-            const destinationLocationGeometry = destinationLocation?.geometry.type === 'Point' ? destinationLocation?.geometry.coordinates : destinationLocation?.properties.anchor.coordinates;
-            mapsIndoorsInstance.getMapView().setCenter({ lat: destinationLocationGeometry[1], lng: destinationLocationGeometry[0] });
+            if (directions?.destinationLocation) {
+                // Get the destination location
+                const destinationLocation = directions?.destinationLocation;
 
-            // Check if the solution allows the zoom level to be 22.
-            // If yes, set the zoom level to 22, otherwise set it to 21.
-            mapsindoors.services.SolutionsService.getSolution().then(solution => {
-                const hasZoom22 = Object.values(solution.modules).find(zoomLevel => zoomLevel === 'z22')
-                mapsIndoorsInstance?.setZoom(hasZoom22 ? 22 : 21);
-            });
+                // Center the map to the location coordinates.
+                const destinationLocationGeometry = destinationLocation?.geometry.type === 'Point' ? destinationLocation?.geometry.coordinates : destinationLocation?.properties.anchor.coordinates;
+                mapsIndoorsInstance.getMapView().setCenter({ lat: destinationLocationGeometry[1], lng: destinationLocationGeometry[0] });
+
+                // Check if the solution allows the zoom level to be 22.
+                // If yes, set the zoom level to 22, otherwise set it to 21.
+                mapsindoors.services.SolutionsService.getSolution().then(solution => {
+                    const hasZoom22 = Object.values(solution.modules).find(zoomLevel => zoomLevel === 'z22')
+                    mapsIndoorsInstance?.setZoom(hasZoom22 ? 22 : 21);
+                });
+            }
         }
+
     }, [activeStep]);
 
     /**
-     * Navigate to the next step.
+     * Navigate to the next step.s
      * Set the previous step in order to show the correct
      * instruction and travel mode.
      */
