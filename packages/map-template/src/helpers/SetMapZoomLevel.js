dbbb3a006e047d74e99201458d61c4cfee84fb63
Andreea Ceachir <113503166+andreeaceachir142@users.noreply.github.com>
2023-06-23 09:39:05 +0200

    Update packages/map-template/src/helpers/SetMapZoomLevel.js
    Co-authored-by: Anders Mogensen <am@mapspeople.com>

--- packages/map-template/src/helpers/SetMapZoomLevel.js
+++ packages/map-template/src/helpers/SetMapZoomLevel.js
@@ -4,7 +4,7 @@ const mapsindoors = window.mapsindoors;
  * Check if the solution allows the zoom level to be 22.
  * If yes, set the zoom level to 22, otherwise set it to 21.
  */
-export default function SetMapZoomLevel(mapsIndoorsInstance) {
+export default function setMapZoomLevel(mapsIndoorsInstance) {
     return mapsindoors.services.SolutionsService.getSolution().then(solution => {
         const hasZoom22 = Object.values(solution.modules).find(zoomLevel => zoomLevel === 'z22')
         mapsIndoorsInstance?.setZoom(hasZoom22 ? 22 : 21);
