9eaab67ce63240178003ed854e91816070a9e1d6
Andreea Ceachir <ance@mapspeople.com>
2023-09-18 13:06:38 +0200

    chore: Rename icon.

--- packages/map-template/src/components/Wayfinding/Wayfinding.jsx
+++ packages/map-template/src/components/Wayfinding/Wayfinding.jsx
@@ -36,7 +36,7 @@ const searchFieldIdentifiers = {
     FROM: 'FROM'
 };
 
-const googlePlacesIcon = "data:image/svg+xml,%3Csvg width='10' height='10' viewBox='0 0 14 20' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M7 0C3.13 0 0 3.13 0 7C0 12.25 7 20 7 20C7 20 14 12.25 14 7C14 3.13 10.87 0 7 0ZM7 9.5C5.62 9.5 4.5 8.38 4.5 7C4.5 5.62 5.62 4.5 7 4.5C8.38 4.5 9.5 5.62 9.5 7C9.5 8.38 8.38 9.5 7 9.5Z' fill='black' fill-opacity='0.88'/%3E%3C/svg%3E%0A"
+const locationMarkerIcon = "data:image/svg+xml,%3Csvg width='10' height='10' viewBox='0 0 14 20' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M7 0C3.13 0 0 3.13 0 7C0 12.25 7 20 7 20C7 20 14 12.25 14 7C14 3.13 10.87 0 7 0ZM7 9.5C5.62 9.5 4.5 8.38 4.5 7C4.5 5.62 5.62 4.5 7 4.5C8.38 4.5 9.5 5.62 9.5 7C9.5 8.38 8.38 9.5 7 9.5Z' fill='black' fill-opacity='0.88'/%3E%3C/svg%3E%0A"
 
 /**
  * Show the wayfinding view.
@@ -428,7 +428,7 @@ function Wayfinding({ onStartDirections, onBack, directionsToLocation, direction
                         {searchResults.map(location =>
                             <ListItemLocation
                                 key={location.id}
-                                icon={(location.properties.type === 'google_places' || location.properties.type === 'mapbox_places') ? googlePlacesIcon : undefined}
+                                icon={(location.properties.type === 'google_places' || location.properties.type === 'mapbox_places') ? locationMarkerIcon : undefined}
                                 location={location}
                                 locationClicked={e => locationClickHandler(e)} />
                         )}
