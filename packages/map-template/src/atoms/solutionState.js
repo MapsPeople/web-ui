68b29070a558242569dafd287efc1254a5de9e7e
Anders Mogensen <am@mapspeople.com>
2023-11-23 14:57:15 +0100

    refactor: clean up.

--- packages/map-template/src/atoms/solutionState.js
+++ packages/map-template/src/atoms/solutionState.js
@@ -3,7 +3,7 @@ import { atom } from 'recoil';
 const solutionState = atom({
     key: 'solution',
     default: null,
-    dangerouslyAllowMutability: true // Solution info needs mutability.
+    dangerouslyAllowMutability: true // Solution info needs mutability since it contains methods.
 });
 
 export default solutionState;
--- packages/map-template/src/hooks/useSetMaxZoomLevel.js
+++ packages/map-template/src/hooks/useSetMaxZoomLevel.js
@@ -1,30 +1,23 @@
-
-import { useEffect, useRef } from 'react';
 import { useRecoilValue } from 'recoil';
 import mapsIndoorsInstanceState from '../atoms/mapsIndoorsInstanceState';
 import solutionState from '../atoms/solutionState';
 
+/**
+ * Custom hook to zoom far in on the map based on Solution module.
+ */
 const useSetMaxZoomLevel = () => {
 
     const mapsIndoorsInstance = useRecoilValue(mapsIndoorsInstanceState);
     const solution = useRecoilValue(solutionState);
-    const maxZoomLevel = useRef();
-    const mapsIndoorsRef = useRef();
-
-    useEffect(() => {
-        if (mapsIndoorsInstance && solution) {
-            mapsIndoorsRef.current = mapsIndoorsInstance;
-            // Set max zoom level to 22 if allowed for the solution, otherwise 21.
-            maxZoomLevel.current = solution.modules.map(module => module.toLowerCase()).includes('z22') ? 22 : 21;
-        }
-    }, [mapsIndoorsInstance, solution]);
 
     const setMaxZoomLevel = () => {
-        // Zoom in as far as allowed (21 or 22).
-        mapsIndoorsRef.current.setZoom(maxZoomLevel.current);
-    };
+        if (mapsIndoorsInstance && solution) {
+            const hasZoom22 = solution.modules.map(module => module.toLowerCase()).includes('z22');
+            mapsIndoorsInstance.setZoom(hasZoom22 ? 22 : 21);
+        }
+    }
 
     return setMaxZoomLevel;
-}
+};
 
 export default useSetMaxZoomLevel;
