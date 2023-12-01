e2e72433d0f202ac836b3072736ea8037854702b
andreeaceachir <ance@mapspeople.com>
2023-12-01 10:58:03 +0100

    chore: Add logic for handling QR code URLs.

--- packages/map-template/src/App.jsx
+++ packages/map-template/src/App.jsx
@@ -8,6 +8,10 @@ function App() {
             <MapsIndoorsMap supportsUrlParameters={true}
                 gmApiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}
                 mapboxAccessToken={import.meta.env.VITE_MAPBOX_ACCESS_TOKEN} 
+                apiKey='c6260c238cee496b8a0453e9'
+                kioskOriginLocationId='4ed6b21ddcc54f61a18ec7b1'
+                primaryColor='#FFF000'
+                logo='https://images.g2crowd.com/uploads/product/image/social_landscape/social_landscape_7a75ff13f42605422950b411ab7e03b5/mapspeople.png'
                 />
         </div>
     );
--- packages/map-template/src/components/QRCodeDialog/QRCodeDialog.jsx
+++ packages/map-template/src/components/QRCodeDialog/QRCodeDialog.jsx
@@ -4,31 +4,97 @@ import './QRCodeDialog.scss';
 import showQRCodeDialogState from "../../atoms/showQRCodeDialogState";
 import primaryColorState from "../../atoms/primaryColorState";
 import apiKeyState from "../../atoms/apiKeyState";
-import venuesState from "../../atoms/venuesState";
 import currentLocationState from "../../atoms/currentLocationState";
 import QRCode from 'qrcode';
 import kioskLocationState from "../../atoms/kioskLocationState";
+import gmApiKeyState from "../../atoms/gmApiKeyState";
+import mapboxAccessTokenState from "../../atoms/mapboxAccessTokenState";
+import logoState from "../../atoms/logoState";
 
 function QRCodeDialog() {
 
     const [, setShowQRCodeDialog] = useRecoilState(showQRCodeDialogState);
     const primaryColor = useRecoilValue(primaryColorState);
     const apiKey = useRecoilValue(apiKeyState);
-    const venue = useRecoilValue(venuesState);
-    const directionsFrom = useRecoilValue(currentLocationState);
-    const directionsTo = useRecoilValue(kioskLocationState);
+    const directionsFrom = useRecoilValue(kioskLocationState);
+    const directionsTo = useRecoilValue(currentLocationState);
+    const gmApiKey = useRecoilValue(gmApiKeyState);
+    const mapboxAccessToken = useRecoilValue(mapboxAccessTokenState);
+    const logo = useRecoilValue(logoState);
+
 
     useEffect(() => {
         if (directionsFrom && directionsTo) {
-            console.log(directionsTo, directionsFrom)
-            const appUrl = `https://map.mapsindoors.com/?apiKey=${apiKey}&venue=${venue}&directionsFrom=${directionsTo.id}&directionsTo=${directionsFrom.id}`;
+            let QRCodeURL;
+
+            let currentUrl = window.location.origin;
+            const currentParams = new URLSearchParams(window.location.search);
+
+            const newParams = new URLSearchParams();
+
+            if (mapboxAccessToken || gmApiKey || currentParams.has('gmApiKey') || currentParams.has('mapboxAccessToken')) {
+                if (currentParams.has('apiKey')) {
+                    const apiKeyParameter = currentParams.get('apiKey')
+                    newParams.append('apiKey', apiKeyParameter)
+
+                    // Check for primary color query parameter
+                    if (currentParams.has('primaryColor')) {
+                        const primaryColorParameter = currentParams.get('primaryColor')
+                        newParams.append('primaryColor', primaryColorParameter)
+                        // Check for primary color property 
+                        // Remove the "#" character from the hex code
+                    } else if (primaryColor) {
+                        newParams.append('primaryColor', primaryColor.replace("#", ""))
+                    }
+
+                    // Check for logo query parameter
+                    if (currentParams.has('logo')) {
+                        const logoParameter = currentParams.get('logo')
+                        newParams.append('logo', logoParameter)
+                        // Check for logo property
+                    } else if (logo) {
+                        newParams.append('logo', logo)
+                    }
+
+                    console.log('params', newParams.toString())
+                    const finalParams = newParams.toString()
+
+                    QRCodeURL = `${currentUrl}/?${finalParams}&directionsFrom=${directionsFrom.id}&directionsTo=${directionsTo.id}`
+                } else {
+                     // Check for primary color query parameter
+                     if (currentParams.has('primaryColor')) {
+                        const primaryColorParameter = currentParams.get('primaryColor')
+                        newParams.append('primaryColor', primaryColorParameter)
+                        // Check for primary color property 
+                        // Remove the "#" character from the hex code
+                    } else if (primaryColor) {
+                        newParams.append('primaryColor', primaryColor.replace("#", ""))
+                    }
+
+                    // Check for logo query parameter
+                    if (currentParams.has('logo')) {
+                        const logoParameter = currentParams.get('logo')
+                        newParams.append('logo', logoParameter)
+                        // Check for logo property
+                    } else if (logo) {
+                        newParams.append('logo', logo)
+                    }
+
+                    const finalParams = newParams.toString()
+
+                    QRCodeURL = `${currentUrl}/?apiKey=${apiKey}&directionsFrom=${directionsFrom.id}&directionsTo=${directionsTo.id}&${finalParams}`
+                }
+            } else {
+                console.log('no mapbox access token or google maps api key provided')
+            }
+
             const options = {
                 errorCorrectionLevel: 'L',
                 margin: 0,
                 width: '225'
             };
 
-            QRCode.toDataURL(appUrl, options)
+            QRCode.toDataURL(QRCodeURL, options)
                 .then((dataUrl) => {
                     document.getElementById('qr').src = dataUrl;
                 }).catch(() => {
