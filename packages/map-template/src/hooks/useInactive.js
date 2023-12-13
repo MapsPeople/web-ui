a1147afee9a38c60754fbaca86d77e214aab3afd
Anders Mogensen <am@mapspeople.com>
2023-12-13 07:42:47 +0100

    feat: add hook to determine if user is inactive.

---
+++ packages/map-template/src/hooks/useInactive.js
@@ -0,0 +1,59 @@
+import { useEffect, useState } from 'react';
+import { useRecoilValue } from 'recoil';
+import isMapReadyState from '../atoms/isMapReadyState';
+import timeoutState from '../atoms/timoutState';
+
+/**
+ * Detect user inactivity and sets state to be inactive after a specified amount of seconds.
+ *
+ * Heavily inspired by useIdle from https://www.npmjs.com/package/@uidotdev/usehooks
+ *
+ * @param {number} seconds
+ * @returns {boolean}
+ */
+export function useInactive() {
+    const [isInactive, setIsInactive] = useState(false);
+
+    const isMapReady = useRecoilValue(isMapReadyState);
+    const timeout = useRecoilValue(timeoutState);
+
+    useEffect(() => {
+        if (!isMapReady || !timeout) {
+            return;
+        }
+
+        let timer;
+
+        const handleUserEvent = (e) => {
+            setIsInactive(false);
+
+            window.clearTimeout(timer);
+            timer = window.setTimeout(handleTimeout, timeout*1000);
+        };
+
+        const handleTimeout = () => {
+            setIsInactive(true);
+        };
+
+        timer = window.setTimeout(handleTimeout, timeout*1000);
+
+        window.addEventListener('mousemove', handleUserEvent);
+        window.addEventListener('mousedown', handleUserEvent);
+        window.addEventListener('resize', handleUserEvent);
+        window.addEventListener('keydown', handleUserEvent);
+        window.addEventListener('touchstart', handleUserEvent);
+        window.addEventListener('wheel', handleUserEvent);
+
+        return () => {
+            window.removeEventListener('mousemove', handleUserEvent);
+            window.removeEventListener('mousedown', handleUserEvent);
+            window.removeEventListener('resize', handleUserEvent);
+            window.removeEventListener('keydown', handleUserEvent);
+            window.removeEventListener('touchstart', handleUserEvent);
+            window.removeEventListener('wheel', handleUserEvent);
+            window.clearTimeout(timer);
+        };
+    }, [timeout, isMapReady]);
+
+    return isInactive;
+}
