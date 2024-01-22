1b42df95cf44b4dcc86d28b5a479adad604f64e6
andreeaceachir <ance@mapspeople.com>
2024-01-22 14:37:11 +0100

    feat: Update default values.

--- packages/map-template/src/components/MapsIndoorsMap/MapsIndoorsMap.jsx
+++ packages/map-template/src/components/MapsIndoorsMap/MapsIndoorsMap.jsx
@@ -42,8 +42,8 @@ function MapsIndoorsMap(props) {
         const queryStringParams = new URLSearchParams(queryString);
 
         const defaultProps = {
-            apiKey: '3ddemo',
-            venue: 'WEWORK',
+            apiKey: '02c329e6777d431a88480a09',
+            venue: 'AUSTIN',
             logo: 'https://app.mapsindoors.com/mapsindoors/gfx/mapspeople-logo/mapspeople-pin.svg',
             primaryColor: '#005655', // --brand-colors-dark-pine-100 from MIDT
             useMapProviderModule: false,
