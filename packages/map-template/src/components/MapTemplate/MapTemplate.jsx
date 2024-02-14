338b8af8b83cb86dd2effcd85046e524995f6246
andreeaceachir <ance@mapspeople.com>
2024-02-14 10:43:46 +0100

    chore: Add new RC.

--- packages/map-template/src/components/MapTemplate/MapTemplate.jsx
+++ packages/map-template/src/components/MapTemplate/MapTemplate.jsx
@@ -158,7 +158,7 @@ function MapTemplate({ apiKey, gmApiKey, mapboxAccessToken, venue, locationId, p
 
             const miSdkApiTag = document.createElement('script');
             miSdkApiTag.setAttribute('type', 'text/javascript');
-            miSdkApiTag.setAttribute('src', 'https://app.mapsindoors.com/mapsindoors/js/sdk/4.29.0/mapsindoors-4.29.0.js.gz');
+            miSdkApiTag.setAttribute('src', 'https://app.mapsindoors.com/mapsindoors/js/sdk/CandidateReleases/4.29.4-rc1/mapsindoors-4.29.4-rc1.js.gz');
             document.body.appendChild(miSdkApiTag);
             miSdkApiTag.onload = () => {
                 resolve();
