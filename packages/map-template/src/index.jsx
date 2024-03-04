5c543d021d16970576cda8c573127d52e37651e0
Mark Hougaard <mark@hougaard.email>
2024-03-04 15:32:01 +0100

    chore: Formatting

--- packages/map-template/src/index.jsx
+++ packages/map-template/src/index.jsx
@@ -7,38 +7,38 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 Sentry.init({
-dsn: "https://0ee7fa162023d958c96db25e99c8ff6c@o351128.ingest.sentry.io/4506851619831808",
-// Set environment to localhost if the url includes it. Otherwise, set to production.
-environment: window.location.hostname === 'localhost' ? 'localhost' : 'production',
-integrations: [
-    // See docs for support of different versions of variation of react router
-    // https://docs.sentry.io/platforms/javascript/guides/react/configuration/integrations/react-router/
-    // Sentry.reactRouterV6BrowserTracingIntegration({
-    //         useEffect: React.useEffect,
-    //         useLocation,
-    //         useNavigationType,
-    //         createRoutesFromChildren,
-    //         matchRoutes
-    // }),
-    Sentry.replayIntegration()
-],
+    dsn: "https://0ee7fa162023d958c96db25e99c8ff6c@o351128.ingest.sentry.io/4506851619831808",
+    // Set environment to localhost if the url includes it. Otherwise, set to production.
+    environment: window.location.hostname === 'localhost' ? 'localhost' : 'production',
+    integrations: [
+        // See docs for support of different versions of variation of react router
+        // https://docs.sentry.io/platforms/javascript/guides/react/configuration/integrations/react-router/
+        // Sentry.reactRouterV6BrowserTracingIntegration({
+        //     useEffect: React.useEffect,
+        //     useLocation,
+        //     useNavigationType,
+        //     createRoutesFromChildren,
+        //     matchRoutes
+        // }),
+        Sentry.replayIntegration()
+    ],
 
-// Set tracesSampleRate to 1.0 to capture 100%
-// of transactions for performance monitoring.
-tracesSampleRate: 1.0,
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    tracesSampleRate: 1.0,
 
-// Set `tracePropagationTargets` to control for which URLs distributed tracing should be enabled
-tracePropagationTargets: ["localhost", /^https:\/\/api\.mapsindoors\.com/],
+    // Set `tracePropagationTargets` to control for which URLs distributed tracing should be enabled
+    tracePropagationTargets: ["localhost", /^https:\/\/api\.mapsindoors\.com/],
 
-// Capture Replay for 10% of all sessions,
-// plus for 100% of sessions with an error
-replaysSessionSampleRate: 0.1,
-replaysOnErrorSampleRate: 1.0,
+    // Capture Replay for 10% of all sessions,
+    // plus for 100% of sessions with an error
+    replaysSessionSampleRate: 0.1,
+    replaysOnErrorSampleRate: 1.0,
 });
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-        <App />
+    <App />
 );
 
 // If you want to start measuring performance in your app, pass a function
