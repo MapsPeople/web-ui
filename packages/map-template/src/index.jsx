701b3d164945d07609b3c34b8474563f2cbe78e1
Mark Hougaard <mark@hougaard.email>
2024-03-04 15:28:06 +0100

    chore: Comment out irrelevant parts from Sentry integration

--- packages/map-template/src/index.jsx
+++ packages/map-template/src/index.jsx
@@ -1,24 +1,26 @@
 import * as Sentry from "@sentry/react";
 import React from 'react';
-import ReactDOM, { createRoutesFromChildren, matchRoutes, useLocation, useNavigationType } from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
+// import { createRoutesFromChildren, matchRoutes, useLocation, useNavigationType } from 'react-dom/client';
 import App from './App';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 Sentry.init({
 dsn: "https://0ee7fa162023d958c96db25e99c8ff6c@o351128.ingest.sentry.io/4506851619831808",
+// Set environment to localhost if the url includes it. Otherwise, set to production.
 environment: window.location.hostname === 'localhost' ? 'localhost' : 'production',
 integrations: [
-        // See docs for support of different versions of variation of react router
-        // https://docs.sentry.io/platforms/javascript/guides/react/configuration/integrations/react-router/
-        Sentry.reactRouterV6BrowserTracingIntegration({
-                useEffect: React.useEffect,
-                useLocation,
-                useNavigationType,
-                createRoutesFromChildren,
-                matchRoutes
-        }),
-        Sentry.replayIntegration()
+    // See docs for support of different versions of variation of react router
+    // https://docs.sentry.io/platforms/javascript/guides/react/configuration/integrations/react-router/
+    // Sentry.reactRouterV6BrowserTracingIntegration({
+    //         useEffect: React.useEffect,
+    //         useLocation,
+    //         useNavigationType,
+    //         createRoutesFromChildren,
+    //         matchRoutes
+    // }),
+    Sentry.replayIntegration()
 ],
 
 // Set tracesSampleRate to 1.0 to capture 100%
