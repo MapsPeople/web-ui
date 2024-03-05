9216a6d66ae5024141b198b5005e3bba3e5a1a16
Mark Hougaard <mark@hougaard.email>
2024-03-05 11:44:13 +0100

    chore: Moved Sentry to MapsIndoorsMap.jsx to use for the Web Component

--- packages/map-template/src/components/MapsIndoorsMap/MapsIndoorsMap.jsx
+++ packages/map-template/src/components/MapsIndoorsMap/MapsIndoorsMap.jsx
@@ -1,4 +1,6 @@
-import { useEffect, useState } from 'react';
+import * as Sentry from "@sentry/react";
+import React, { useEffect, useState } from 'react';
+import { createRoutesFromChildren, matchRoutes, useLocation, useNavigationType } from 'react-dom/client';
 import { RecoilRoot } from 'recoil';
 import MapTemplate from '../MapTemplate/MapTemplate.jsx';
 
@@ -110,6 +112,36 @@ function MapsIndoorsMap(props) {
         </RecoilRoot>
     )
 }
+
+Sentry.init({
+    dsn: "https://0ee7fa162023d958c96db25e99c8ff6c@o351128.ingest.sentry.io/4506851619831808",
+    // Set environment to localhost if the url includes it. Otherwise, set to production.
+    environment: window.location.hostname === 'localhost' ? 'localhost' : 'production',
+    integrations: [
+        // See docs for support of different versions of variation of react router
+        // https://docs.sentry.io/platforms/javascript/guides/react/configuration/integrations/react-router/
+        Sentry.reactRouterV6BrowserTracingIntegration({
+            useEffect: React.useEffect,
+            useLocation,
+            useNavigationType,
+            createRoutesFromChildren,
+            matchRoutes
+        }),
+        Sentry.replayIntegration()
+    ],
+
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    tracesSampleRate: 1.0,
+
+    // Set `tracePropagationTargets` to control for which URLs distributed tracing should be enabled
+    tracePropagationTargets: ["localhost", /^https:\/\/api\.mapsindoors\.com/],
+
+    // Capture Replay for 10% of all sessions,
+    // plus for 100% of sessions with an error
+    replaysSessionSampleRate: 0.1,
+    replaysOnErrorSampleRate: 1.0,
+});
 
 export default MapsIndoorsMap;
 
