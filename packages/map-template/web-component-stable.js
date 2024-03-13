21a06e73d420de67a698b6731513c606adc6428d
Mark Hougaard <mark@hougaard.email>
2024-03-13 22:43:45 +0100

    feat: Add cdn-files

---
+++ packages/map-template/web-component-stable.js
@@ -0,0 +1,2 @@
+import MapsindoorsMap from 'https://cdn.jsdelivr.net/npm/@mapsindoors/map-template@stable/dist/mapsindoors-webcomponent.es.min.js';
+window.customElements.define('mapsindoors-map', MapsindoorsMap)
