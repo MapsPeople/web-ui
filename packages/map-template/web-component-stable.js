4003272bd8390ce4eff6644a6620b73e8c3ef12f
Mark Hougaard <mark@hougaard.email>
2024-03-18 14:17:26 +0100

    chore: Remove cdn-files again

--- packages/map-template/web-component-stable.js
+++
@@ -1,2 +0,0 @@
-import MapsindoorsMap from 'https://cdn.jsdelivr.net/npm/@mapsindoors/map-template@stable/dist/mapsindoors-webcomponent.es.min.js';
-window.customElements.define('mapsindoors-map', MapsindoorsMap)
