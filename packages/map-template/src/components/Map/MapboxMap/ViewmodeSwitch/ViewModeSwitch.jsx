c66ab8070ee4e2500f77699f8ff867c14ff53163
Anders Mogensen <am@mapspeople.com>
2024-05-07 15:31:00 +0200

    feat: new component for switching between 2D and 3D on a Mapbox map.

---
+++ packages/map-template/src/components/Map/MapboxMap/ViewmodeSwitch/ViewModeSwitch.jsx
@@ -0,0 +1,73 @@
+import { useEffect, useState } from 'react';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import solutionState from '../../../../atoms/solutionState';
+import { ReactComponent as Light2D } from '../../../../assets/2d-light.svg';
+import { ReactComponent as Dark2D } from '../../../../assets/2d-dark.svg';
+import { ReactComponent as Light3D } from '../../../../assets/3d-light.svg';
+import { ReactComponent as Dark3D } from '../../../../assets/3d-dark.svg';
+import primaryColorState from '../../../../atoms/primaryColorState';
+import mapboxViewModeState from '../../../../atoms/mapboxViewModeState';
+import './ViewModeSwitch.scss';
+
+export const ViewModes = Object.freeze({
+    'initial3D': 1,
+    'clicked3D': 2,
+    'clicked2D': 3
+});
+
+function ViewModeSwitch({ mapView }) {
+
+    const solution = useRecoilValue(solutionState);
+    const primaryColor = useRecoilValue(primaryColorState);
+    const [viewMode, setViewMode] = useRecoilState(mapboxViewModeState);
+    const [shouldShowSwitch, setShouldShowSwitch] = useState(false);
+
+
+    useEffect(() => {
+        if (['mapbox', '3dwalls', 'floorplan'].every(requiredModule => solution.modules.map(module => module.toLowerCase()).includes(requiredModule))) {
+            setShouldShowSwitch(true);
+            setViewMode(ViewModes.initial3D);
+        }
+    }, [solution]);
+
+    useEffect(() => {
+        // Toggle 2D and 3D features on the map
+        if (mapView?.isReady) {
+            switch (viewMode) {
+                case ViewModes.clicked2D:
+                    mapView.hideFeatures([mapView.MapboxFeatures.MODEL3D, mapView.MapboxFeatures.WALLS3D, mapView.MapboxFeatures.EXTRUSION3D, mapView.MapboxFeatures.EXTRUDEDBUILDINGS]);
+                    mapView.tilt(0, 2000);
+                    break;
+                case ViewModes.initial3D:
+                case ViewModes.clicked3D:
+                    mapView.tilt(45, 2000);
+                    mapView.hideFeatures([mapView.MapboxFeatures.MODEL2D, mapView.MapboxFeatures.WALLS2D]);
+                    break;
+                default:
+                    // Intentionally left blank
+            }
+        }
+
+    }, [mapView?.isReady, viewMode]);
+
+    return (
+        <>
+            {shouldShowSwitch && <div className="switch">
+                <button className="switch__button"
+                    onClick={() => setViewMode(ViewModes.clicked2D)}
+                    style={{ backgroundColor: viewMode === ViewModes.clicked2D ? primaryColor : 'white' }}
+                >
+                    {viewMode === ViewModes.clicked2D ? <Light2D /> : <Dark2D />}
+                </button>
+                <button className="switch__button"
+                    onClick={() => setViewMode(ViewModes.clicked3D)}
+                    style={{ backgroundColor: [ViewModes.initial3D, ViewModes.clicked3D].includes(viewMode) ? primaryColor : 'white' }}
+                >
+                    {[ViewModes.initial3D, ViewModes.clicked3D].includes(viewMode) ? <Light3D /> : <Dark3D />}
+                </button>
+            </div>}
+        </>
+    )
+}
+
+export default ViewModeSwitch;
