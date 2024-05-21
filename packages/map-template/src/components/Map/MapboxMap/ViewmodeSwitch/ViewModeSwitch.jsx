e789e60310592f5fc3278c671c64ed91b9dcd38d
andreeaceachir <ance@mapspeople.com>
2024-05-21 13:36:59 +0200

    chore: Add explaining comments.

--- packages/map-template/src/components/Map/MapboxMap/ViewmodeSwitch/ViewModeSwitch.jsx
+++ packages/map-template/src/components/Map/MapboxMap/ViewmodeSwitch/ViewModeSwitch.jsx
@@ -11,8 +11,6 @@ import './ViewModeSwitch.scss';
 import currentPitchSelector from '../../../../selectors/currentPitch';
 import { ViewModes } from '../../../../constants/viewModes';
 
-
-
 function ViewModeSwitch({ mapView }) {
 
     const solution = useRecoilValue(solutionState);
@@ -21,6 +19,7 @@ function ViewModeSwitch({ mapView }) {
     const [shouldShowSwitch, setShouldShowSwitch] = useState(false);
     const currentPitch = useRecoilValue(currentPitchSelector);
 
+    // If the modules are enables, show the Visibility Switch and set the View Mode
     useEffect(() => {
         if (['mapbox', '3dwalls', 'floorplan'].every(requiredModule => solution.modules.map(module => module.toLowerCase()).includes(requiredModule))) {
             setShouldShowSwitch(true);
@@ -28,18 +27,22 @@ function ViewModeSwitch({ mapView }) {
         }
     }, [solution]);
 
+    // Handle the toggling of the 2D/3D features on the map
     useEffect(() => {
-        // Toggle 2D and 3D features on the map
         if (mapView?.isReady) {
             switch (viewMode) {
+                    // If the 2D View Mode has been clicked, hide the 3D features and tilt the map to 0 degrees. 
                 case ViewModes.clicked2D:
-                    mapView.hideFeatures([mapView.MapboxFeatures.MODEL3D, mapView.MapboxFeatures.WALLS3D, mapView.MapboxFeatures.EXTRUSION3D, mapView.MapboxFeatures.EXTRUDEDBUILDINGS]);
                     mapView.tilt(0, 2000);
+                    mapView.hideFeatures([mapView.MapboxFeatures.MODEL3D, mapView.MapboxFeatures.WALLS3D, mapView.MapboxFeatures.EXTRUSION3D, mapView.MapboxFeatures.EXTRUDEDBUILDINGS]);
                     break;
+                    // If the Visibility Switch has not been interacted with, hide the 2D features
+                    // Tilt the map to the 'currentPitch' value - this is the value that the timeout property resets to. 
                 case ViewModes.initial3D:
                     mapView.tilt(currentPitch, 2000);
                     mapView.hideFeatures([mapView.MapboxFeatures.MODEL2D, mapView.MapboxFeatures.WALLS2D]);
                     break;
+                    // If the 3D View Mode has been clicked, hide the 2D features and tilt the map to 45 degrees. 
                 case ViewModes.clicked3D:
                     mapView.tilt(45, 2000);
                     mapView.hideFeatures([mapView.MapboxFeatures.MODEL2D, mapView.MapboxFeatures.WALLS2D]);
--- packages/map-template/src/constants/viewModes.js
+++ packages/map-template/src/constants/viewModes.js
@@ -1,3 +1,4 @@
+// Constant holding the values of the 2D/3D Visibility Switch.
 export const ViewModes = Object.freeze({
     initial3D: 1,
     clicked3D: 2,
