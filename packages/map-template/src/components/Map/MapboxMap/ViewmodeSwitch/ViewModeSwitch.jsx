eb69f3e7f30d5f251acbc4a6c5b9f07abc9f991a
andreeaceachir <ance@mapspeople.com>
2024-06-07 11:12:59 +0200

    chore: Update SDK version and function name.

--- packages/map-template/src/components/Map/MapboxMap/ViewmodeSwitch/ViewModeSwitch.jsx
+++ packages/map-template/src/components/Map/MapboxMap/ViewmodeSwitch/ViewModeSwitch.jsx
@@ -38,18 +38,18 @@ function ViewModeSwitch({ mapView }) {
                     // If the 2D View Mode has been clicked, hide the 3D features and tilt the map to 0 degrees. 
                 case ViewModes.clicked2D:
                     mapView.tilt(0, 2000);
-                    mapView.hideFeatures([mapView.MapboxFeatures.MODEL3D, mapView.MapboxFeatures.WALLS3D, mapView.MapboxFeatures.EXTRUSION3D, mapView.MapboxFeatures.EXTRUDEDBUILDINGS]);
+                    mapView.hideFeatures([mapView.FeatureType.MODEL3D, mapView.FeatureType.WALLS3D, mapView.FeatureType.EXTRUSION3D, mapView.FeatureType.EXTRUDEDBUILDINGS]);
                     break;
                     // If the Visibility Switch has not been interacted with, hide the 2D features
                     // Tilt the map to the 'currentPitch' value - this is the value that the timeout property resets to. 
                 case ViewModes.initial3D:
                     mapView.tilt(currentPitch, 2000);
-                    mapView.hideFeatures([mapView.MapboxFeatures.MODEL2D, mapView.MapboxFeatures.WALLS2D]);
+                    mapView.hideFeatures([mapView.FeatureType.MODEL2D, mapView.FeatureType.WALLS2D]);
                     break;
                     // If the 3D View Mode has been clicked, hide the 2D features and tilt the map to 45 degrees. 
                 case ViewModes.clicked3D:
                     mapView.tilt(45, 2000);
-                    mapView.hideFeatures([mapView.MapboxFeatures.MODEL2D, mapView.MapboxFeatures.WALLS2D]);
+                    mapView.hideFeatures([mapView.FeatureType.MODEL2D, mapView.FeatureType.WALLS2D]);
                     break;
                 default:
                     // Intentionally left blank
