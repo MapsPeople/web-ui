b0ee30ea2148cda20d59957eee382f16af52973a
Mateusz Banaszak <matb@mapspeople.com>
2024-07-30 12:39:59 +0200

    SCOUTING!

--- packages/components/src/components/scroll-buttons/scroll-buttons.tsx
+++ packages/components/src/components/scroll-buttons/scroll-buttons.tsx
@@ -8,9 +8,14 @@ import { Component, h, JSX, Method, Prop, Watch } from '@stencil/core';
 export class ScrollButtons {
     /**
      * Reference to the element with scroll on parent element.
+     *
      * @type {HTMLDivElement}
      */
     @Prop() scrollContainerElementRef: HTMLDivElement;
+
+    /**
+     * Watch for container scroll events.
+     */
     @Watch('scrollContainerElementRef')
     addScrollEventListener(): void {
         this.resizeObserver?.disconnect();
@@ -36,12 +41,16 @@ export class ScrollButtons {
         this.addScrollEventListener();
     }
 
+    /**
+     * Disconnects ResizeObserver.
+     */
     disconnectedCallback(): void {
         this.resizeObserver?.disconnect();
     }
 
     /**
      * Determines how far to scroll when clicking one of the buttons. Default value is 100.
+     *
      * @type {number}
      */
     @Prop() scrollLength = 100;
@@ -53,6 +62,7 @@ export class ScrollButtons {
 
     /**
      * Updates enable/disable state for scroll up and down buttons.
+     *
      * @returns {Promise<void>}
      */
     @Method()
@@ -74,6 +84,7 @@ export class ScrollButtons {
 
     /**
      * Update scroll position.
+     *
      * @param {number} value - Value to scroll.
      */
     updateScrollPosition(value: number): void {
@@ -87,6 +98,11 @@ export class ScrollButtons {
         }
     }
 
+    /**
+     * Render scoll buttons.
+     *
+     * @returns {JSX.Element}
+     */
     render(): JSX.Element {
         return (
             <div part="container" class="scroll-buttons">
@@ -94,15 +110,19 @@ export class ScrollButtons {
                     type="button"
                     disabled
                     aria-label="Scroll Up"
-                    ref={(el) => this.upButtonElement = el as HTMLButtonElement}
-                    onClick={() => this.updateScrollPosition(-this.scrollLength)}>
+                    ref={(el: HTMLButtonElement | null): void => {
+                        this.upButtonElement = el as HTMLButtonElement | null;
+                    }}
+                    onClick={(): void => this.updateScrollPosition(-this.scrollLength)}>
                     <mi-icon icon-name="chevron-up" />
                 </button>
                 <button part="button button-down" class="mi-button mi-button--base btn btn-down"
                     type="button"
                     aria-label="Scroll Down"
-                    ref={(el) => this.downButtonElement = el as HTMLButtonElement}
-                    onClick={() => this.updateScrollPosition(this.scrollLength)}>
+                    ref={(el: HTMLButtonElement | null): void => {
+                        this.downButtonElement = el as HTMLButtonElement | null;
+                    }}
+                    onClick={(): void => this.updateScrollPosition(this.scrollLength)}>
                     <mi-icon icon-name="chevron-down" />
                 </button>
             </div>
