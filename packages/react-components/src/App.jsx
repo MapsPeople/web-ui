b6f0803ec4e5d72de3efc4f613295e7a2b8655f8
Anders Mogensen <am@mapspeople.com>
2024-07-30 14:53:01 +0200

    chore: use the Map component in components test app. Use env for access token.

--- packages/react-components/src/App.jsx
+++ packages/react-components/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Map from './components/Map/Map';
 
 function App() {
     return (
@@ -6,7 +7,12 @@ function App() {
             <h1>MapsIndoors React Components</h1>
             <hr />
             <h2>Map</h2>
-            ...
+            <div className="map-container">
+                <Map
+                    apiKey="mapspeople3d"
+                    mapboxAccessToken={import.meta.env.VITE_MAPBOX_ACCESS_TOKEN}
+                />
+            </div>
         </div>
     )
 }
