c10341152e88464c1b32ba2babba407762e440fd
Mateusz Banaszak <matb@mapspeople.com>
2024-08-14 09:38:50 +0200

    chore: change naming to DOM

--- packages/map-template/src/components/Search/Search.jsx
+++ packages/map-template/src/components/Search/Search.jsx
@@ -143,8 +143,8 @@ function Search({ onSetSize, isOpen }) {
 
         // Handles updates to scroll buttons when the category changes.
         // When a category changes, the scroll buttons need to have their enabled/disabled states updated.
-        // Since some categories might load before the SCSS element is fully rendered, we listen for the 'transitionend' event.
-        // The 'transitionend' event is triggered when the SCSS element changes its size, which can occur as a result of new categories being fetched.
+        // Since some categories might load before the DOM element is fully rendered, we listen for the 'transitionend' event.
+        // The 'transitionend' event is triggered when the DOM element changes its size, which can occur as a result of new categories being fetched.
         // Upon completion of the size transition, the 'newCategorySelected' function is triggered to handle the updated state.
         searchRef.current?.addEventListener('transitionend', () => {
             scrollButtonsRef?.current?.newCategorySelected(locations);
