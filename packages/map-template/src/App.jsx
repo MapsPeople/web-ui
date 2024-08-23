f5977660fdf70b204e42e88e2da3a2830b53459c
andreeaceachir <ance@mapspeople.com>
2024-08-23 13:34:23 +0200

    chore: Add all missing props.

--- packages/map-template/src/App.jsx
+++ packages/map-template/src/App.jsx
@@ -10,6 +10,11 @@ function App() {
                 gmApiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}
                 mapboxAccessToken={import.meta.env.VITE_MAPBOX_ACCESS_TOKEN}
                 apiKey="4dabfbf824f247afad1ee8d2"
+                startZoomLevel={17}
+                pitch={70}
+                bearing={45}
+                logo="https://www.milcorp.com/assets/Dream-Force-logo.png"
+                primaryColor="#00A0E0"
                 />
         </div>
     );
