bd4b6c290269692d066d59bcb10a35be3e9bc86a
andreeaceachir <ance@mapspeople.com>
2024-09-12 14:27:29 +0200

    chore: Add remaining props.

--- packages/map-template/src/App.jsx
+++ packages/map-template/src/App.jsx
@@ -9,6 +9,12 @@ function App() {
             <MapsIndoorsMap supportsUrlParameters={true}
                 gmApiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}
                 mapboxAccessToken="pk.eyJ1IjoibWFwc3Blb3BsZSIsImEiOiJjbTB6ODFmemwwMngxMmlzZ3Y5NjQybGNxIn0.-vG9FP8YUKjNcR-7JD0_Ng"
+                apiKey="feebac92011b4eda83381dbc"
+                startZoomLevel="19"
+                pitch={45}
+                bearing={300}
+                logo="https://upload.wikimedia.org/wikipedia/en/d/d9/AUH_airport_logo.png"
+                primaryColor="#120D74"
                 />
         </div>
     );
