5eacdfed5b112277d9ad8f7d5830f6829d9f64cb
andreeaceachir <ance@mapspeople.com>
2024-09-12 13:47:15 +0200

    chore: Add mapbox access token.

--- packages/map-template/src/App.jsx
+++ packages/map-template/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
             {/* This is the Map Template component */}
             <MapsIndoorsMap supportsUrlParameters={true}
                 gmApiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}
-                mapboxAccessToken={import.meta.env.VITE_MAPBOX_ACCESS_TOKEN}
+                mapboxAccessToken="pk.eyJ1IjoibWFwc3Blb3BsZSIsImEiOiJjbTB6ODFmemwwMngxMmlzZ3Y5NjQybGNxIn0.-vG9FP8YUKjNcR-7JD0_Ng"
                 />
         </div>
     );
