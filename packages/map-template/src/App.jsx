bcb89376c91f6b8f9948f106569d2eab1836f0de
andreeaceachir <ance@mapspeople.com>
2024-09-13 11:06:24 +0200

    chore: Fixed Access Token.

--- packages/map-template/src/App.jsx
+++ packages/map-template/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
             {/* This is the Map Template component */}
             <MapsIndoorsMap supportsUrlParameters={true}
                 gmApiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}
-                mapboxAccessToken={pk.eyJ1IjoibWFwc3Blb3BsZSIsImEiOiJjbTB5MWdxdXYwYWxpMmtvY3d5MXh0bmY3In0.J_rjm6Ujqb25wKHAmTLHrw}
+                mapboxAccessToken='pk.eyJ1IjoibWFwc3Blb3BsZSIsImEiOiJjbTB5MWdxdXYwYWxpMmtvY3d5MXh0bmY3In0.J_rjm6Ujqb25wKHAmTLHrw'
                 apiKey="5465afc63a3148b7b852bc4a"
                 />
         </div>
