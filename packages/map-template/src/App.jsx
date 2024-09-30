1fa41b870e2d29e77e075801a44f19c278619b2a
andreeaceachir <ance@mapspeople.com>
2024-09-30 20:49:43 +0200

    chore: Update parameters.

--- packages/map-template/src/App.jsx
+++ packages/map-template/src/App.jsx
@@ -11,6 +11,8 @@ function App() {
                 apiKey="5c30a42387294aa895c83bb1"
                 logo='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.GwY9O6V1XitdxF5UMGxh8gHaDr%26pid%3DApi&f=1&ipt=683dfb4a14781b0933c7145005c258c0fd28ed7a3525ab023bd22bfe38edf122&ipo=images'
                 primaryColor="#A71930"
+                miTransitionLevel={10}
+                showRoadNames={true}
                 />
         </div>
     );
