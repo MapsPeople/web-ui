587e638ffd7edf924e83f354cbe3e8e3e49f17c5
Anders Mogensen <am@mapspeople.com>
2024-10-02 15:48:25 +0200

    feat: implement basic maps for Mapbox and Google Maps.

--- packages/react-components/src/App.jsx
+++ packages/react-components/src/App.jsx
@@ -5,10 +5,16 @@ function App() {
     return (
         <>
             <h1>MapsIndoors React Components</h1>
-            <hr />
-            <MIMap
-                apiKey="mapspeople3d"
-            />
+            <h2><code>&lt;MIMap /&gt;</code></h2>
+            <div style={{ marginLeft: '5vw', width: "90vw", height: "70vh", boxShadow: '0px 0px 20px #000' }}>
+                <MIMap
+                    apiKey="mapspeople3d"
+                    gmApiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}
+                    mapboxAccessToken={import.meta.env.VITE_MAPBOX_ACCESS_TOKEN}
+                    center={{ lat: 57.05825378109134, lng: 9.951020621279326 }}
+                    zoom={19.2}
+                />
+            </div>
         </>
     )
 }
--- packages/react-components/src/components/MIMap/MIMap.jsx
+++ packages/react-components/src/components/MIMap/MIMap.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
+import MapboxMap from './MapboxMap/MapboxMap';
+import GoogleMapsMap from './GoogleMapsMap/GoogleMapsMap';
 
 const mapTypes = {
     Google: 'google',
@@ -9,10 +11,12 @@ const mapTypes = {
 MIMap.propTypes = {
     apiKey: PropTypes.string.isRequired,
     gmApiKey: PropTypes.string,
-    mapboxAccessToken: PropTypes.string
+    mapboxAccessToken: PropTypes.string,
+    center: PropTypes.object,
+    zoom: PropTypes.number
 }
 
-function MIMap({ apiKey, gmApiKey, mapboxAccessToken }) {
+function MIMap({ apiKey, gmApiKey, mapboxAccessToken, center, zoom }) {
 
     const [mapType, setMapType] = useState();
 
@@ -21,6 +25,26 @@ function MIMap({ apiKey, gmApiKey, mapboxAccessToken }) {
             window.mapsindoors.MapsIndoors.setMapsIndoorsApiKey(apiKey);
         }
     }, [apiKey]);
+
+    const onMapViewInitialized = (mapView) => {
+        // Instantiate MapsIndoors instance
+        const mi = new window.mapsindoors.MapsIndoors({
+            mapView
+        });
+
+        // Detect when the mouse hovers over a location and store the hovered location
+        // If the location is non-selectable, remove the hovering by calling the unhoverLocation() method.
+        mi.on('mouseenter', () => {
+            const hoveredLocation = mi.getHoveredLocation()
+
+            if (hoveredLocation?.properties.locationSettings?.selectable === false) {
+                mi.unhoverLocation();
+            }
+        });
+
+         // TODO: Turn off visibility for building outline for demo purposes until the SDK supports Display Rules for Buildings too.
+         mi.setDisplayRule(['MI_BUILDING_OUTLINE'], { visible: false });
+    }
 
     /*
      * Determine map type based on the given map provider tokens.
@@ -34,8 +58,8 @@ function MIMap({ apiKey, gmApiKey, mapboxAccessToken }) {
     }, [gmApiKey, mapboxAccessToken]);
 
     return <>
-        {mapType === mapTypes.Google && <p>Google Maps</p>}
-        {mapType === mapTypes.Mapbox && <p>Mapbox</p>}
+        {mapType === mapTypes.Google && <GoogleMapsMap apiKey={gmApiKey} onInitialized={onMapViewInitialized} center={center} zoom={zoom} />}
+        {mapType === mapTypes.Mapbox && <MapboxMap accessToken={mapboxAccessToken} onInitialized={onMapViewInitialized} center={center} zoom={zoom} />}
     </>
 }
 
