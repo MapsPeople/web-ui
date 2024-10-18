bf7339793fdc08bbc86c6c5b26655fd0c908e8b1
Anders Mogensen <am@mapspeople.com>
2024-10-18 09:56:15 +0200

    feat: expose MapsIndoors instance as function prop and showcase that it works by setting up a MI click event listener.

--- packages/react-components/src/App.jsx
+++ packages/react-components/src/App.jsx
@@ -1,7 +1,22 @@
+import { useEffect, useState } from 'react';
 import './App.css'
 import MIMap from './components/MIMap/MIMap'
 
 function App() {
+
+    const [mapsIndoorsInstance, setMapsIndoorsInstance] = useState();
+
+    useEffect(() => {
+        if (mapsIndoorsInstance) {
+            // For demo purpose: setup MapsIndoors click event listener that sets clicked location to be invisible.
+            mapsIndoorsInstance.on('click', (location) => {
+                const displayRule = mapsIndoorsInstance.getDisplayRule(location);
+                displayRule.visible = false;
+                mapsIndoorsInstance.setDisplayRule(location.id, displayRule);
+            });
+        }
+    }, [mapsIndoorsInstance]);
+
     return (
         <>
             <h1>MapsIndoors React Components</h1>
@@ -13,6 +28,7 @@ function App() {
                     mapboxAccessToken={import.meta.env.VITE_MAPBOX_ACCESS_TOKEN}
                     center={{ lat: 57.05825378109134, lng: 9.951020621279326 }}
                     zoom={19.2}
+                    onMapsIndoorsInstanceReady={miInstance => setMapsIndoorsInstance(miInstance)}
                 />
             </div>
         </>
