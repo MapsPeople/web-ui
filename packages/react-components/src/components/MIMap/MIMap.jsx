12329cc7d95a8d10f48ad4aee17ee305fe9b9fba
Anders Mogensen <am@mapspeople.com>
2024-10-28 10:20:57 +0100

    feat: add support for a bearing property to control the map.

--- packages/react-components/src/components/MIMap/MIMap.jsx
+++ packages/react-components/src/components/MIMap/MIMap.jsx
@@ -19,6 +19,7 @@ MIMap.propTypes = {
     mapboxAccessToken: PropTypes.string,
     center: PropTypes.object,
     zoom: PropTypes.number,
+    bearing: PropTypes.number,
     mapOptions: PropTypes.object,
     onMapsIndoorsInstanceReady: PropTypes.func
 }
@@ -30,11 +31,12 @@ MIMap.propTypes = {
  * @param {string} props.mapboxAccessToken - Mapbox Access Token if you want to show a Mapbox map.
  * @param {Object} props.center - Object with latitude and longitude on which the map will center. Example: { lat: 55, lng: 10 }
  * @param {number} props.zoom - Zoom level for the map.
+ * @param {number} props.bearing - The bearing of the map (rotation from north) as a number. Not recommended for Google Maps with 2D Models.
  * @param {Object} props.mapOptions - Options for instantiating and styling the map.
  * @param {function} props.onMapsIndoorsInstanceReady - Callback for when the MapsIndoors instance (https://app.mapsindoors.com/mapsindoors/js/sdk/latest/docs/mapsindoors.MapsIndoors.html) is ready. The instance is given as payload.
  * @returns
  */
-function MIMap({ apiKey, gmApiKey, mapboxAccessToken, center, zoom, mapOptions, onMapsIndoorsInstanceReady }) {
+function MIMap({ apiKey, gmApiKey, mapboxAccessToken, center, zoom, bearing, mapOptions, onMapsIndoorsInstanceReady }) {
 
     const [mapType, setMapType] = useState();
     const [mapsIndoorsInstance, setMapsIndoorsInstance] = useState();
@@ -83,8 +85,8 @@ function MIMap({ apiKey, gmApiKey, mapboxAccessToken, center, zoom, mapOptions, 
     }, [gmApiKey, mapboxAccessToken]);
 
     return <>
-        {mapType === mapTypes.Google && <GoogleMapsMap mapsIndoorsInstance={mapsIndoorsInstance} apiKey={gmApiKey} onInitialized={onMapViewInitialized} center={center} zoom={zoom} mapOptions={mapOptions} />}
-        {mapType === mapTypes.Mapbox && <MapboxMap mapsIndoorsInstance={mapsIndoorsInstance} accessToken={mapboxAccessToken} onInitialized={onMapViewInitialized} center={center} zoom={zoom} mapOptions={mapOptions} />}
+        {mapType === mapTypes.Google && <GoogleMapsMap mapsIndoorsInstance={mapsIndoorsInstance} apiKey={gmApiKey} onInitialized={onMapViewInitialized} center={center} zoom={zoom} mapOptions={mapOptions} heading={bearing} />}
+        {mapType === mapTypes.Mapbox && <MapboxMap mapsIndoorsInstance={mapsIndoorsInstance} accessToken={mapboxAccessToken} onInitialized={onMapViewInitialized} center={center} zoom={zoom} mapOptions={mapOptions} bearing={bearing} />}
     </>
 }
 
