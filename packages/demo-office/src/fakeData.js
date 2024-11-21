fc0be020a6106d25cb1a80eabd4f57453a828172
Anders Mogensen <am@mapspeople.com>
2024-11-21 10:30:38 +0100

    chore: create a fake data set that for now holds a fake device position.

---
+++ packages/demo-office/src/fakeData.js
@@ -0,0 +1,9 @@
+/*
+ * This file is used to provide fake data for the demo.
+ */
+export default {
+    devicePosition: {
+        lat: 30.360573677092603,
+        lng: -97.74225010031331,
+    }
+};
