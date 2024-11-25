2d8f5bbd74f2d1b7b64e90b47827c05d114f1c88
andreeaceachir <ance@mapspeople.com>
2024-11-25 16:14:38 +0100

    feat: Upgrade SDK version.

--- packages/map-template/src/components/MapTemplate/MapTemplate.jsx
+++ packages/map-template/src/components/MapTemplate/MapTemplate.jsx
@@ -180,8 +180,8 @@ function MapTemplate({ apiKey, gmApiKey, mapboxAccessToken, venue, locationId, p
 
             const miSdkApiTag = document.createElement('script');
             miSdkApiTag.setAttribute('type', 'text/javascript');
-            miSdkApiTag.setAttribute('src', 'https://app.mapsindoors.com/mapsindoors/js/sdk/4.36.3/mapsindoors-4.36.3.js.gz');
-            miSdkApiTag.setAttribute('integrity', 'sha384-UPPUHq6fu2VwcT5Rv+1+FzOlAg6mLd8iAUpr6taDU5XsKNluEpmIenfqKoEBQZPc');
+            miSdkApiTag.setAttribute('src', 'https://app.mapsindoors.com/mapsindoors/js/sdk/4.37.0/mapsindoors-4.37.0.js.gz');
+            miSdkApiTag.setAttribute('integrity', 'sha384-YToxFQP0mxSe8mF3Qm0Nvfu9eZ11Ky9MHCbOhrYgX5/9aoCUteGbUIE/IO1Uot/P');
             miSdkApiTag.setAttribute('crossorigin', 'anonymous');
             document.body.appendChild(miSdkApiTag);
             miSdkApiTag.onload = () => {
