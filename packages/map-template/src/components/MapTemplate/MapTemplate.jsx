e34b23292ecfc65146e439a9eaa47f468b647b34
andreeaceachir <ance@mapspeople.com>
2024-11-28 14:11:20 +0100

    chore: Update to RC 9.

--- packages/map-template/src/components/MapTemplate/MapTemplate.jsx
+++ packages/map-template/src/components/MapTemplate/MapTemplate.jsx
@@ -177,8 +177,8 @@ function MapTemplate({ apiKey, gmApiKey, mapboxAccessToken, venue, locationId, p
 
             const miSdkApiTag = document.createElement('script');
             miSdkApiTag.setAttribute('type', 'text/javascript');
-            miSdkApiTag.setAttribute('src', 'https://app.mapsindoors.com/mapsindoors/js/sdk/CandidateReleases/2024.09.11-rc7/mapsindoors-2024.9.11-rc7.js.gz');
-            miSdkApiTag.setAttribute('integrity', 'sha384-bG7DYBlJzo+qS513CT3apmOujDg6/WuG0zgwawBOT/cj6enGORcJiIfIhNIAQuJj');
+            miSdkApiTag.setAttribute('src', 'https://app.mapsindoors.com/mapsindoors/js/sdk/CandidateReleases/2024.09.11-rc9/mapsindoors-2024.9.11-rc9.js.gz');
+            miSdkApiTag.setAttribute('integrity', 'sha384-6bsPKx+zk38xfO5uN5D4MR8lTg0xIzzE8WtGyxgXH0M/ZriqklrAe+SJmbJpIOLz');
             // miSdkApiTag.setAttribute('src', 'https://app.mapsindoors.com/mapsindoors/js/sdk/4.36.2/mapsindoors-4.36.2.js.gz');
             // miSdkApiTag.setAttribute('integrity', 'sha384-S/xO5B5WzQeUYCK1QJE26gX50/Q++SiOzkp8JI6A4Cf/okBpWZ+YK3WcdXkcdzHK');
             miSdkApiTag.setAttribute('crossorigin', 'anonymous');
