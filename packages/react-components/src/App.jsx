487f43ce81a9f4d037f3449c50702c82e0a1214b
Anders Mogensen <am@mapspeople.com>
2024-12-04 12:05:53 +0100

    chore: setup Vite project.

---
+++ packages/react-components/src/App.jsx
@@ -0,0 +1,9 @@
+import './App.css'
+function App() {
+    return (
+        <>
+            <h1>MapsIndoors React Components</h1>
+        </>
+    )
+}
+export default App;
---
+++ packages/react-components/vite.config.js
@@ -0,0 +1,28 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import eslint from 'vite-plugin-eslint';
+import svgr from 'vite-plugin-svgr';
+
+
+export default defineConfig(() => {
+    return {
+        build: {
+            outDir: 'build',
+            sourcemap: true,
+        },
+        plugins: [
+            react(),
+            svgr(),
+            eslint(),
+            sentryVitePlugin({
+                org: process.env.SENTRY_ORG,
+                project: process.env.SENTRY_PROJECT,
+
+                // Auth tokens can be obtained from https://sentry.io/orgredirect/organizations/:orgslug/settings/auth-tokens/
+                authToken: process.env.SENTRY_AUTH_TOKEN,
+                reactComponentAnnotation: { enabled: true },
+            }),
+        ]
+    }
+});
