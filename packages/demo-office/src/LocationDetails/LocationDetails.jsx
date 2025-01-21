45c8edb735c0cab22755f899983c6a70dd4f6ae0
Anders Mogensen <am@mapspeople.com>
2025-01-21 11:06:01 +0100

    feat: expose a onHeightChanged callback that will fire whenever the Location prop changes and will pass on the height of the rendered element.

--- packages/demo-office/src/LocationDetails/LocationDetails.jsx
+++ packages/demo-office/src/LocationDetails/LocationDetails.jsx
@@ -1,10 +1,12 @@
 import './LocationDetails.css';
 import PropTypes from 'prop-types';
 import { ReactComponent as CloseIcon } from '../assets/close.svg';
+import { useEffect, useRef } from 'react';
 
 LocationDetails.propTypes = {
     location: PropTypes.object,
-    onRequestClose: PropTypes.func
+    onRequestClose: PropTypes.func,
+    onHeightChanged: PropTypes.func
 };
 
 /**
@@ -13,8 +15,11 @@ LocationDetails.propTypes = {
  * @param {object} props
  * @param {object} props.location - The MapsIndoors Location object.
  * @param {function} props.onRequestClose - Callback function to request closing the Location details editor.
+ * @param {function} props.onHeightChanged - Callback function to notify the parent component that the height of the Location details editor has changed.
  */
-function LocationDetails({ location, onRequestClose }) {
+function LocationDetails({ location, onRequestClose, onHeightChanged }) {
+
+    const myRef = useRef(null);
 
     /**
      * Fire callback to request closing the Location details editor
@@ -25,7 +30,18 @@ function LocationDetails({ location, onRequestClose }) {
         }
     }
 
-    return location && <div className="location-details">
+    /*
+     * When the location prop changes, we want to calculate the height of the Location details editor.
+     * This is used to calculate padding on the map, so that the Location details editor does not overlap the map.
+     */
+    useEffect(() => {
+        const rect = myRef.current?.getBoundingClientRect();
+        if (rect && typeof onHeightChanged === 'function') {
+            onHeightChanged(rect.height ?? 0);
+        }
+    }, [location]);
+
+    return location && <div ref={myRef} className="location-details">
         <div className="location-details__header">
             <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M20 10c0 6-8 12-8 12s-8-6-8-12a8 8 0 0 1 16 0Z"></path><circle cx="12" cy="10" r="3"></circle></svg>
             <h1 className="location-details__heading">
