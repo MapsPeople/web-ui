2b78604af3387d68a628da68e51080a67a6bbcdc
Anders Mogensen <am@mapspeople.com>
2025-01-22 10:39:42 +0100

    refactor: better naming.

--- packages/demo-office/src/LocationDetails/LocationDetails.jsx
+++ packages/demo-office/src/LocationDetails/LocationDetails.jsx
@@ -19,7 +19,7 @@ LocationDetails.propTypes = {
  */
 function LocationDetails({ location, onRequestClose, onHeightChanged }) {
 
-    const myRef = useRef(null);
+    const elementRef = useRef(null);
 
     /**
      * Fire callback to request closing the Location details editor
@@ -35,13 +35,13 @@ function LocationDetails({ location, onRequestClose, onHeightChanged }) {
      * This is used to calculate padding on the map, so that the Location details editor does not overlap the map.
      */
     useEffect(() => {
-        const rect = myRef.current?.getBoundingClientRect();
+        const rect = elementRef.current?.getBoundingClientRect();
         if (rect && typeof onHeightChanged === 'function') {
             onHeightChanged(rect.height ?? 0);
         }
     }, [location]);
 
-    return location && <div ref={myRef} className="location-details">
+    return location && <div ref={elementRef} className="location-details">
         <div className="location-details__header">
             <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M20 10c0 6-8 12-8 12s-8-6-8-12a8 8 0 0 1 16 0Z"></path><circle cx="12" cy="10" r="3"></circle></svg>
             <h1 className="location-details__heading">
