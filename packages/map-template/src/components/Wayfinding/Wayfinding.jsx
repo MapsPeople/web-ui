18c775ebc921e24e6c928b80e542faf4ef42bb90
Anders Mogensen <am@mapspeople.com>
2025-02-26 10:31:02 +0100

    refactor: set bottomsheet size to max when showing search results and dont focus on field since we want to indicate that you can also click on Location s on the map to select.

--- packages/map-template/src/components/Wayfinding/Wayfinding.jsx
+++ packages/map-template/src/components/Wayfinding/Wayfinding.jsx
@@ -179,6 +179,7 @@ function Wayfinding({ onStartDirections, onBack, directionsToLocation, direction
             setSearchResults(results);
             const resultsHaveGooglePlaces = results.filter(result => result.properties.type === 'google_places').length > 0;
             setHasGooglePlaces(resultsHaveGooglePlaces);
+            setSize(snapPoints.MAX);
         }
     }
 
@@ -397,19 +398,6 @@ function Wayfinding({ onStartDirections, onBack, directionsToLocation, direction
         }
 
         if (isActive && !fromFieldRef.current?.getValue()) {
-            // Set focus on the from field.
-            // But wait for any bottom sheet transition to end before doing that to avoid content jumping when virtual keyboard appears.
-            const sheet = wayfindingRef.current.closest('.sheet');
-            if (sheet) {
-                sheet.addEventListener('transitionend', () => {
-                    if (directionsFromLocation !== 'USER_POSITION_PENDING' && directionsToLocation?.id !== 'USER_POSITION') {
-                        fromFieldRef.current.focusInput();
-                    }
-                }, { once: true });
-            } else if (directionsFromLocation !== 'USER_POSITION_PENDING' && directionsToLocation?.id !== 'USER_POSITION') {
-                fromFieldRef.current.focusInput();
-            }
-
             if (userPosition && !originLocation && directionsToLocation?.id !== 'USER_POSITION' && !originLocationWasSet) {
                 // If the user's position is known and no origin location is set, use the position as Origin.
 
