b7536d07ffb5a4cb4efc44a1748e0b604f1dec5d
Lucaci-Andrei <anlu@mapspeople.com>
2025-03-21 12:52:21 +0100

    fix: add npm package version to process.env in the vite config

--- packages/map-template/vite.config.js
+++ packages/map-template/vite.config.js
@@ -8,6 +8,9 @@ import svgr from 'vite-plugin-svgr';
 
 export default defineConfig(() => {
     return {
+        define: {
+            'process.env.npm_package_version': JSON.stringify(process.env.npm_package_version)
+        },
         server: {
             port: 3000
         },
@@ -23,7 +26,7 @@ export default defineConfig(() => {
             sentryVitePlugin({
                 org: process.env.SENTRY_ORG,
                 project: process.env.SENTRY_PROJECT,
-          
+
                 // Auth tokens can be obtained from https://sentry.io/orgredirect/organizations/:orgslug/settings/auth-tokens/
                 authToken: process.env.SENTRY_AUTH_TOKEN,
                 reactComponentAnnotation: { enabled: true },
