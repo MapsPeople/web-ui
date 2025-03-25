2597c95c234b235341d0122415f97f8e5f86424c
andreeaceachir <ance@mapspeople.com>
2025-03-25 11:52:22 +0100

    chore: Upgrade to latest SDK version.

--- packages/map-template/src/components/MapTemplate/MapTemplate.jsx
+++ packages/map-template/src/components/MapTemplate/MapTemplate.jsx
@@ -221,8 +221,8 @@ function MapTemplate({ apiKey, gmApiKey, mapboxAccessToken, venue, locationId, p
 
             const miSdkApiTag = document.createElement('script');
             miSdkApiTag.setAttribute('type', 'text/javascript');
-            miSdkApiTag.setAttribute('src', 'https://app.mapsindoors.com/mapsindoors/js/sdk/4.38.6/mapsindoors-4.38.6.js.gz');
-            miSdkApiTag.setAttribute('integrity', 'sha384-KgcghquvjXEHX5M0Z6ZxykdoQqrLI7vfijQxuGYuafr1q3NNWaNNiXgFYeb6ZawZ');
+            miSdkApiTag.setAttribute('src', 'https://app.mapsindoors.com/mapsindoors/js/sdk/4.39.1/mapsindoors-4.39.1.js.gz');
+            miSdkApiTag.setAttribute('integrity', 'sha384-GpJDp6n8LKQE3YZvZJa7pTRet98vRLs4GHxIIORQ06IESv4WZ5KkanelZ2EjCnts');
             miSdkApiTag.setAttribute('crossorigin', 'anonymous');
             document.body.appendChild(miSdkApiTag);
             miSdkApiTag.onload = () => {
