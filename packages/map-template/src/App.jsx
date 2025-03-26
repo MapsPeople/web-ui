48b819697614e0ded52214877c119d29b695cf38
Lucaci-Andrei <anlu@mapspeople.com>
2025-03-26 14:00:11 +0100

    feat: wrap App component in Sentry.ErrorBoundary for better error handling

--- packages/map-template/src/App.jsx
+++ packages/map-template/src/App.jsx
@@ -4,13 +4,15 @@ import MapsIndoorsMap from './components/MapsIndoorsMap/MapsIndoorsMap';
 
 function App() {
     return (
-        <div className="app">
-            {/* This is the Map Template component */}
-            <MapsIndoorsMap supportsUrlParameters={true}
-                gmApiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}
-                mapboxAccessToken={import.meta.env.VITE_MAPBOX_ACCESS_TOKEN}
+        <Sentry.ErrorBoundary>
+            <div className="app">
+                {/* This is the Map Template component */}
+                <MapsIndoorsMap supportsUrlParameters={true}
+                    gmApiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}
+                    mapboxAccessToken={import.meta.env.VITE_MAPBOX_ACCESS_TOKEN}
                 />
-        </div>
+            </div>
+        </Sentry.ErrorBoundary>
     );
 }
 
