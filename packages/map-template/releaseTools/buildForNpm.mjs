41588dd2f23d2ced2e1d37d97ca13473e40a1f7b
Lucaci-Andrei <anlu@mapspeople.com>
2025-03-26 10:56:29 +0100

    feat: integrate Sentry plugin for error tracking and add sourcemaps when building for npm

--- packages/map-template/releaseTools/buildForNpm.mjs
+++ packages/map-template/releaseTools/buildForNpm.mjs
@@ -4,14 +4,30 @@ import { build } from 'vite';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
+import { sentryVitePlugin } from "@sentry/vite-plugin";
+import { loadEnv } from 'vite';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const env = loadEnv('production', process.cwd(), '');
+
+// Define Sentry plugin configuration
+const sentryPlugin = sentryVitePlugin({
+    org: env.VITE_SENTRY_ORG,
+    project: env.VITE_SENTRY_PROJECT,
+    authToken: env.VITE_SENTRY_AUTH_TOKEN,
+    release: {
+        name: `map-template@${process.env.npm_package_version}`
+    },
+    setCommits: false,
+    reactComponentAnnotation: { enabled: true },
+});
 
 const libraries = [
     // Web Component
     {
         build: {
             outDir: './dist',
+            sourcemap: true,
             lib: {
                 entry: path.resolve(__dirname, 'webcomponent.js'),
                 name: 'MapTemplate', // unused, but required
@@ -27,16 +43,19 @@ const libraries = [
             }
         },
         plugins: [
-            cssInjectedByJsPlugin()
+            cssInjectedByJsPlugin(),
+            sentryPlugin
         ],
         define: {
-            'process.env.NODE_ENV': '"production"'
+            'process.env.NODE_ENV': '"production"',
+            'process.env.npm_package_version': JSON.stringify(process.env.npm_package_version)
         }
     },
     // React Component
     {
         build: {
             outDir: './dist',
+            sourcemap: true,
             lib: {
                 entry: path.resolve(__dirname, 'reactcomponent.js'),
                 name: 'MapTemplateReact', // unused, but required
@@ -55,7 +74,8 @@ const libraries = [
             }
         },
         plugins: [
-            cssInjectedByJsPlugin()
+            cssInjectedByJsPlugin(),
+            sentryPlugin
         ]
     }
 ]
