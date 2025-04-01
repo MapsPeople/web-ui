23ebf26a9c78d510803e564a3ced7808cc7c7d66
Anders Mogensen <am@mapspeople.com>
2025-04-01 12:33:40 +0200

    refactor: part 1 of many in refactoring the bottom sheet in order to reduce tech debt and eliminate some small bugs: swipe handling and setting sheet height.

--- packages/map-template/src/components/BottomSheet/Sheet/Sheet.jsx
+++ packages/map-template/src/components/BottomSheet/Sheet/Sheet.jsx
@@ -4,236 +4,179 @@ import { ContainerContext } from '../ContainerContext';
 import { useSwipeable } from 'react-swipeable';
 import { snapPoints } from '../../../constants/snapPoints';
 import './Sheet.scss';
-import { useRecoilState } from 'recoil';
-import isBottomSheetLoadedState from '../../../atoms/isBottomSheetLoadedState';
 import PropTypes from 'prop-types';
-
-
-let dragStartHeight;
 
 Sheet.propTypes = {
     children: PropTypes.node.isRequired,
     isOpen: PropTypes.bool.isRequired,
-    minHeight: PropTypes.string.isRequired,
-    preferredSizeSnapPoint: PropTypes.number,
-    onSwipedToSnapPoint: PropTypes.func,
-    reFitWhenContentChanges: PropTypes.bool
+    initialSnapPoint: PropTypes.number.isRequired,
+    minimizedHeight: PropTypes.number.isRequired
 };
 
 /**
- * A Sheet for showing content in the bottom of the screen.
- * The sheet height can be changed with swipe gestures.
+ * A Sheet for showing content in the bottom of the screen inside the BottomSheet.
+ *
+ * The user can swipe the sheet up and down to change its height.
+ * The sheet can be set to one of three snap points: MAX, FIT, MIN.
  *
  * @param {Object} props
- * @param {React.ReactNode} props.children - The content to be displayed inside the sheet
+ * @param {React.ReactNode} props.children - The content to be displayed inside the sheet.
  * @param {boolean} props.isOpen - If the sheet is open (visible) or not.
- * @param {number} props.minheight - The minimum height of the sheet. It cannot be resized to below this height.
- * @param {number} props.preferredSizeSnapPoint - Change this to programatically change sheet height to a snap point.
- * @param {function} [props.onSwipedToSnapPoint] - Callback function is run when user swiped to a snap points. Has the snap point as parameter.
+ * @param {number} props.initialSnapPoint - The initial snap point of the sheet.
+ * @param {number} props.minimizedHeight - The minimum height of the sheet. It cannot be resized below this height.
  */
-function Sheet({ children, isOpen, minHeight, preferredSizeSnapPoint, onSwipedToSnapPoint, reFitWhenContentChanges }) {
-
+function Sheet({ children, isOpen, initialSnapPoint, minimizedHeight }) {
     /** Referencing the sheet DOM element */
     const sheetRef = useRef();
 
-    /** Referencing the DOM element of the sheet content div. */
+    /** Referencing the DOM element of the sheet content div, the one containing the children. */
     const contentRef = useRef();
 
-    /** The current size preset ("snap point") */
-    const [currentSnapPoint, setCurrentSnapPoint] = useState(snapPoints.FIT);
-
-    /** True if the sheet is currently being dragged with pointer (mouse, finger) */
-    const [isDragging, setIsDragging] = useState(false);
-
-    /** If user is swiping up or down */
-    const [swipeGestureDirection, setSwipeGestureDirection] = useState();
-
-    /** Reference to the DOM element of the container element of all bottom sheets. */
-    const container = useContext(ContainerContext);
-
-    /** The current height of the content in pixels */
+    /** Tracking last known content height. Used when determining snap points then the user is making a swipe gesture. */
     const contentHeightRef = useRef();
 
+    /** Used to determine if a swipe gesture should count as a swipe up or down */
+    const dragStartHeight = useRef();
+
+    /** Holding the snap point that the sheet height is currently snapped to */
+    const snappedTo = useRef();
+
+    /** CSS styles for both div elements, containing a height */
+    // TODO: Should this just be a height state?
     const [style, setStyle] = useState({});
 
-    const [, setIsBottomSheetLoaded] = useRecoilState(isBottomSheetLoadedState);
-
-    let fitMutationObserver;
+    /** True if the sheet is currently being dragged with pointer (mouse, finger) */
+    const [isDragging, setIsDragging] = useState(false); // eslint-disable-line
 
     /**
-     * Set up a mutation observer to listen to changes in child content.
-     * When changes are detected, we will re-fit the sheet to the content.
+     * Reference to the DOM element of the container element of all bottom sheets.
+     * This is used for eg. a full height bottom sheet.
      */
-    function setupFitMutationObserver() {
-        if (fitMutationObserver) {
-            fitMutationObserver.disconnect();
-        }
-
-        fitMutationObserver = new MutationObserver(() => {
-            const height = contentRef.current.children.item(0).clientHeight;
-            contentHeightRef.current = height;
-            changeSheetHeight(snapPoints.FIT);
-        });
-
-        fitMutationObserver.observe(contentRef.current, {
-            childList: true, // checks if children are added or removed
-            subtree: true // check if descendants are added or removed all the way down the tree of DOM nodes
-        });
-    }
+    const container = useContext(ContainerContext);
 
     /**
-     * Change the height of the sheet to one of the preset sizes (min, fit, max).
+     * Change sheet height to one of the three snap points.
      *
-     * @param {number} targetSize - Which of the sizes to change to.
+     * @param {number} snapPoint
      */
-    function changeSheetHeight(targetSize) {
-        // Prevent going to minimum size state if the content size is the same in order to prevent the need for double swipes to change height.
-        if (targetSize === snapPoints.MIN && contentHeightRef.current <= minHeight) {
-            return;
-        }
-
-        if (currentSnapPoint === snapPoints.FIT) {
-            sheetRef.current.style.height = `${contentHeightRef.current}px`;
-        }
+    function snapSheetHeightToSnapPoint(snapPoint) {
 
         // Set the actual pixel height of the sheet.
         // RequestAnimationFrame is needed in order to the height style change to kick in thus enabling the transition of height.
         // Inspired by https://css-tricks.com/using-css-transitions-auto-dimensions/#aa-technique-3-javascript
         requestAnimationFrame(() => {
-            switch (targetSize) {
+            switch (snapPoint) {
                 case snapPoints.MAX:
                     setStyle({ height: `${container.current.clientHeight}px` });
+                    contentHeightRef.current = container.current.clientHeight;
                     break;
-                case snapPoints.FIT:
-                    setStyle({ height: `${contentHeightRef.current}px` });
+                case snapPoints.FIT: {
+                    // Get the height of the contentRef div
+                    const height = contentRef.current.children.item(0).clientHeight;
+
+                    // if that height is less than the minimized height, set the height to the minimized height
+                    if (height < minimizedHeight) {
+                        setStyle({ height: `${minimizedHeight}px` });
+                        contentHeightRef.current = minimizedHeight;
+                        snapPoint = snapPoints.MIN;
+                    } else {
+                        setStyle({ height: `${height}px` });
+                        contentHeightRef.current = height;
+                    }
+
                     break;
+                }
                 case snapPoints.MIN:
-                    setStyle({ height: `${minHeight}px` });
+                    setStyle({ height: `${minimizedHeight}px` });
+                    contentHeightRef.current = minimizedHeight;
                     break;
                 default:
                     break;
             }
-            setCurrentSnapPoint(targetSize);
         });
 
-        if (typeof onSwipedToSnapPoint === 'function') {
-            // When snap transition has ended, call the callback
-            sheetRef.current.addEventListener('transitionend', () => {
-                onSwipedToSnapPoint(targetSize);
-            }, { once: true });
-        }
+        snappedTo.current = snapPoint;
     }
 
     /**
-     * When the transition has ended, set the "setIsBottomSheetLoaded" atom to true.
-     */
-    useEffect(() => {
-        sheetRef.current.addEventListener('transitionend', () => {
-            setIsBottomSheetLoaded(true);
-        }, { once: true });
-    }, []);
-
-    /**
-     * React to changes in the size prop, meaning something requests
-     * the sheet to be set to a certain snap point.
-     */
-    useEffect(() => {
-        // If the sheet is instructed to fit to content, set up a mutation observer to listen to changes.
-        if (isOpen && reFitWhenContentChanges && preferredSizeSnapPoint === snapPoints.FIT) {
-            setupFitMutationObserver();
-        } else if (reFitWhenContentChanges) {
-            if (fitMutationObserver) {
-                fitMutationObserver.disconnect();
-            }
-        }
-
-        // Do not set the height of the sheet if the preferredSizeSnapPoint is undefined.
-        if (preferredSizeSnapPoint === undefined) {
-            return;
-        }
-
-        requestAnimationFrame(() => {
-            changeSheetHeight(preferredSizeSnapPoint);
-        });
-
-        return () => {
-            if (fitMutationObserver) {
-                fitMutationObserver.disconnect();
-            }
-        };
-    }, [preferredSizeSnapPoint, isOpen]); // eslint-disable-line react-hooks/exhaustive-deps
-
-    /**
      * Handler for swipe gestures on the sheet.
+     *
+     * The handler is created using the useSwipeable hook from the react-swipeable library.
+     * It allows the user to swipe up and down to change the height of the sheet.
      */
     const swipeHandler = useSwipeable({
-        onSwipeStart: (e) => {
+        onSwipeStart: () => {
             setIsDragging(true);
-            setSwipeGestureDirection(e.dir);
-            dragStartHeight = sheetRef.current.clientHeight;
+            dragStartHeight.current = sheetRef.current.clientHeight;
         },
-        onSwiping: (e) => {
-            // Constantly update the sheet height corresponding to the pointer position.
-            const height = Math.max(dragStartHeight - e.deltaY, minHeight);
+        onSwiping: swipeEvent => {
+            const height = Math.max(dragStartHeight.current - swipeEvent.deltaY, minimizedHeight);
             setStyle({ height: `${height}px` });
         },
-        onSwiped: (e) => {
-            // When swiping stopped, snap to a snap point.
-            // The snap point is the next one in the swipe direction if the swiped distance was more than 60px.
-            // Otherwise it will bounce back to the current snap point.
-
+        onSwiped: swipeEvent => {
             setIsDragging(false);
-            const newHeight = Math.max(dragStartHeight - e.deltaY, minHeight);
-
-            let minThreshold, maxThreshold;
-            if (swipeGestureDirection.toUpperCase() === 'DOWN') {
-                minThreshold = contentHeightRef.current - 60;
-                maxThreshold = container.current.clientHeight - 60;
+            if (Math.abs(swipeEvent.deltaY) < 60) {
+                // If the user swiped less than 60 pixels, it doesn't count as a change, and the sheet
+                // should snap back to the current snap point.
+                snapSheetHeightToSnapPoint(snappedTo.current);
             } else {
-                minThreshold = parseInt(minHeight) + 60;
-                maxThreshold = contentHeightRef.current + 60;
-            }
+                // Determine new snapped point based on the swipe direction and the current snap point.
+                let newSnapPoint;
+                if (swipeEvent.dir.toUpperCase() === 'DOWN') {
+                    if (snappedTo.current === snapPoints.MAX) {
+                        newSnapPoint = snapPoints.FIT;
+                    } else if (snappedTo.current === snapPoints.FIT) {
+                        newSnapPoint = snapPoints.MIN;
+                    }
+                } else if (swipeEvent.dir.toUpperCase() === 'UP') {
+                    if (snappedTo.current === snapPoints.MIN) {
+                        newSnapPoint = snapPoints.FIT;
+                    } else if (snappedTo.current === snapPoints.FIT) {
+                        newSnapPoint = snapPoints.MAX;
+                    }
+                }
 
-            let targetSnapPoint;
-            if (newHeight <= minThreshold) {
-                targetSnapPoint = snapPoints.MIN;
-            } else if (newHeight <= maxThreshold) {
-                targetSnapPoint = snapPoints.FIT;
-            } else {
-                targetSnapPoint = snapPoints.MAX;
+                if (newSnapPoint) {
+                    snapSheetHeightToSnapPoint(newSnapPoint);
+                }
             }
-
-            changeSheetHeight(targetSnapPoint);
         },
         trackMouse: true,
         preventScrollOnSwipe: true
     });
 
-    /*
-     * Handle sheet height when children or isOpen change.
-     */
     useEffect(() => {
-        if (isOpen === false) {
-            contentHeightRef.current = minHeight;
-            sheetRef.current.style.height = '';
+        if (isOpen) {
+            snappedTo.current = initialSnapPoint;
+            snapSheetHeightToSnapPoint(initialSnapPoint);
         } else {
-            contentHeightRef.current = contentRef.current.clientHeight;
+            // TODO Anything?
         }
-    }, [children.length, isOpen]);
 
-    /*
-     * Pass through ref to share the ref between component and useSwipeable.
+    }, [minimizedHeight, initialSnapPoint, isOpen]);
+
+
+    /**
+     * Pass through ref to share the ref between the component and the useSwipeable hook.
      */
     const refPassthrough = el => {
         swipeHandler.ref(el);
         sheetRef.current = el;
-    };
+    }
 
-    return <div {...swipeHandler} ref={refPassthrough} style={style} className={`sheet ${isOpen ? 'sheet--active' : ''} ${isDragging ? 'sheet--dragging' : ''}`}>
-        <div ref={contentRef} className="sheet__content" style={style}>
-            {children}
+    return (
+        <div
+            {...swipeHandler}
+            ref={refPassthrough}
+            className={`sheet ${isOpen ? 'sheet--active' : ''} ${isDragging ? 'sheet--dragging' : ''}`}
+            style={style}
+        >
+            {/* We need to have a div in a div for it being able to scroll content separately and for the height to be measurable */}
+            <div ref={contentRef} className="sheet__content" style={style}>
+                {children}
+            </div>
         </div>
-    </div>
+    );
 }
 
 export default Sheet;
