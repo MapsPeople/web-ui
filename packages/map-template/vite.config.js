09aaf186ba17671ca2fb940e73da598306d29822
Lucaci-Andrei <anlu@mapspeople.com>
2025-04-08 08:21:16 +0200

    refactor: load environment variables for Sentry plugin configuration in Vite

--- packages/map-template/vite.config.js
+++ packages/map-template/vite.config.js
@@ -1,10 +1,14 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import eslint from 'vite-plugin-eslint';
 import { ViteFaviconsPlugin } from 'vite-plugin-favicon2';
 import svgr from 'vite-plugin-svgr';
+import { loadEnv } from 'vite';
 
-export default defineConfig(() => {
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '');
+
     return {
         server: {
             port: 3000
@@ -18,6 +22,16 @@ export default defineConfig(() => {
             svgr(),
             ViteFaviconsPlugin('./public/favicon.png'),
             eslint(),
+            sentryVitePlugin({
+                org: env.VITE_SENTRY_ORG,
+                project: env.VITE_SENTRY_PROJECT,
+                authToken: env.VITE_SENTRY_AUTH_TOKEN,
+                release: {
+                    name: `map-template@${process.env.npm_package_version}`
+                },
+                setCommits: false,
+                reactComponentAnnotation: { enabled: true },
+            }),
         ]
     }
 });
