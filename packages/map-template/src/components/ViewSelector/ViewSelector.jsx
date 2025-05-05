eaf74249a187b4a2b902839dec157480b40f5f1b
Lucaci-Andrei <anlu@mapspeople.com>
2025-05-05 10:36:30 +0200

    feat: add base view selector react component

---
+++ packages/map-template/src/components/ViewSelector/ViewSelector.jsx
@@ -0,0 +1,11 @@
+import './ViewSelector.scss';
+
+function ViewSelector() {
+    return (
+        <div className="view-selector">
+            ViewSelector Component
+        </div>
+    );
+}
+
+export default ViewSelector; 
