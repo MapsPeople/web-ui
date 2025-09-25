eea9aa0dad7fe03bdc08acbae76efa1c1ad46fb1
Lucaci-Andrei <anlu@mapspeople.com>
2025-09-25 15:03:05 +0200

    feat: add chat mode icon SVG and integrate it into the Search component for dynamic background manipulation

--- packages/map-template/src/components/WebComponentWrappers/Search/Search.jsx
+++ packages/map-template/src/components/WebComponentWrappers/Search/Search.jsx
@@ -5,6 +5,7 @@ import userPositionState from '../../../atoms/userPositionState';
 import languageState from '../../../atoms/languageState';
 import currentVenueNameState from '../../../atoms/currentVenueNameState';
 import searchAllVenuesState from '../../../atoms/searchAllVenues';
+import isChatModeEnabledState from '../../../atoms/isChatModeEnabledState';
 import PropTypes from 'prop-types';
 
 /**
@@ -40,6 +41,7 @@ const SearchField = forwardRef(function SearchFieldComponent(props, ref) {
     const searchNear = useNear();
 
     const currentVenueName = useRecoilValue(currentVenueNameState);
+    const isChatModeEnabled = useRecoilValue(isChatModeEnabledState);
 
     const searchAllVenues = useRecoilValue(searchAllVenuesState);
 
@@ -128,6 +130,82 @@ const SearchField = forwardRef(function SearchFieldComponent(props, ref) {
         };
     }, [onKeyDown]);
 
+        // Effect to find and manipulate the background-image based on chat mode
+        // TODO: This is a hack to get the chat mode icon to work. We should find a better way to do this.
+        useEffect(() => {
+            if (!elementRef.current) return;
+
+            const findAndUpdateBackgroundImage = async () => {
+                try {
+                    // Get the input element from the web component
+                    const inputElement = await elementRef.current.getInputField();
+                    
+                    if (inputElement) {
+                        if (isChatModeEnabled) {
+                            // Hide the background image when chat mode is enabled
+                            inputElement.style.backgroundImage = 'none';
+                            
+                            // Create and inject chat mode icon
+                            const existingIcon = inputElement.parentElement?.querySelector('.chat-mode-icon');
+                            if (!existingIcon) {
+                                const iconContainer = document.createElement('div');
+                                iconContainer.className = 'chat-mode-icon';
+                                iconContainer.style.cssText = `
+                                    position: absolute;
+                                    left: var(--spacing-medium, 12px);
+                                    top: 50%;
+                                    transform: translateY(-50%);
+                                    z-index: 10;
+                                    pointer-events: none;
+                                    width: 18px;
+                                    height: 18px;
+                                `;
+                                
+                                // Create SVG element
+                                const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+                                svg.setAttribute('width', '18');
+                                svg.setAttribute('height', '18');
+                                svg.setAttribute('viewBox', '0 0 24 24');
+                                svg.setAttribute('fill', 'none');
+                                svg.innerHTML = `
+                                    <path d="M12.0002 4C8.43131 4 5.48489 6.67174 5.05443 10.1237C4.98609 10.6718 4.48642 11.0607 3.93838 10.9923C3.39034 10.924 3.00146 10.4243 3.0698 9.87626C6.2354 5.43564 7.4099 2 12.0002 2C16.9708 2 21.0002 6.02944 21.0002 11C21.0002 13.1253 20.2638 15.0782 19.0321 16.6178L21.7072 19.2929C22.0977 19.6834 22.0977 20.3166 21.7072 20.7071C21.3167 21.0976 20.6835 21.0976 20.293 20.7071L17.6178 18.0319C16.3539 19.0427 14.8114 19.72 13.1238 19.9304C12.5758 19.9988 12.0761 19.6099 12.0078 19.0618C11.9395 18.5138 12.3283 18.0141 12.8764 17.9458C16.3284 17.5154 19.0002 14.5689 19.0002 11C20.0002 7.13401 15.8662 4 12.0002 4Z" fill="#959595" style="fill:#959595;fill:color(display-p3 0.5849 0.5849 0.5849);fill-opacity:1;"/>
+                                    <path d="M7.32918 12.2764C7.14498 11.9079 6.61897 11.9079 6.43477 12.2764L5.45647 14.2329C5.40817 14.3297 5.32968 14.4081 5.23288 14.4565L3.27637 15.4348C2.90788 15.619 2.90788 16.1449 3.27637 16.3292L5.23288 17.3074C5.32968 17.3558 5.40817 17.4343 5.45647 17.531L6.43477 19.4875C6.61897 19.8561 7.14498 19.8561 7.32918 19.4875L8.30748 17.531C8.35578 17.4343 8.43427 17.3558 8.53107 17.3074L10.4876 16.3292C10.8561 16.1449 10.8561 15.619 10.4876 15.4348L8.53107 14.4565C8.43427 14.4081 8.35578 14.3297 8.30748 14.2329L7.32918 12.2764Z" fill="#959595" style="fill:#959595;fill:color(display-p3 0.5849 0.5849 0.5849);fill-opacity:1;"/>
+                                `;
+                                
+                                iconContainer.appendChild(svg);
+                                inputElement.parentElement?.appendChild(iconContainer);
+                            }
+                        } else {
+                            // Restore the original background image when chat mode is disabled
+                            inputElement.style.backgroundImage = '';
+                            
+                            // Remove chat mode icon
+                            const existingIcon = inputElement.parentElement?.querySelector('.chat-mode-icon');
+                            if (existingIcon) {
+                                existingIcon.remove();
+                            }
+                        }
+                    }
+                } catch (error) {
+                    // Fallback: try to find input element directly
+                    const searchElement = elementRef.current;
+                    const inputElement = searchElement.shadowRoot?.querySelector('input') || 
+                                        searchElement.querySelector('input');
+                    
+                    if (inputElement) {
+                        if (isChatModeEnabled) {
+                            inputElement.style.backgroundImage = 'none';
+                        } else {
+                            inputElement.style.backgroundImage = '';
+                        }
+                    }
+                }
+            };
+
+            // Try to update immediately
+            findAndUpdateBackgroundImage();
+        }, [isChatModeEnabled]);
+
     return <mi-search ref={elementRef}
         id-attribute="search"
         placeholder={placeholder}
@@ -139,7 +217,8 @@ const SearchField = forwardRef(function SearchFieldComponent(props, ref) {
         mapbox={mapbox}
         google={google}
         mi-venue={searchAllVenues ? undefined : currentVenueName}
-        language={language} />
+        language={language}
+    />
 });
 
 SearchField.propTypes = {
