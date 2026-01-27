48790ad55bff2f3f4d6167722113db3004cf9a01
Lucaci-Andrei <anlu@mapspeople.com>
2026-01-27 11:18:50 +0100

    fix: add better jsdoc to location consent popup

--- packages/map-template/src/components/ChatWindow/LocationConsentPopup/LocationConsentPopup.jsx
+++ packages/map-template/src/components/ChatWindow/LocationConsentPopup/LocationConsentPopup.jsx
@@ -1,6 +1,23 @@
 import PropTypes from 'prop-types';
 import './LocationConsentPopup.scss';
 
+/**
+ * A popup component that requests user consent for location access.
+ *
+ * @description
+ * This component is displayed when the Assistant needs the user's location
+ * to provide personalized directions and nearby discovery features. Location access is optional;
+ * users can decline and still use the chat by manually specifying a start point.
+ *
+ * The popup presents two options:
+ * - "Allow" - grants sending location data
+ * - "Decline" - skips sending location data
+ *
+ * @param {Object} props
+ * @param {Function} props.onAccept - Callback fired when the user clicks "Allow"
+ * @param {Function} props.onDecline - Callback fired when the user clicks "Decline"
+ * @returns {JSX.Element} The location consent popup component
+ */
 function LocationConsentPopup({ onAccept, onDecline }) {
     return (
         <div className="location-consent-popup">
