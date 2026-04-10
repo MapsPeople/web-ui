977f1912b8d00cf9767a6a345521ba2bcd6cef09
Lucaci-Andrei <anlu@mapspeople.com>
2026-04-10 23:03:51 +0200

    fix: streamline mobile padding calculation by removing unnecessary checks and simplifying observer logic

--- packages/map-template/src/helpers/GetMobilePaddingBottom.js
+++ packages/map-template/src/helpers/GetMobilePaddingBottom.js
@@ -4,35 +4,21 @@
  */
 export default function getMobilePaddingBottom() {
     return new Promise(resolve => {
-        const bottomSheet = document.querySelector('.sheet--active');
-        const mapContainer = document.querySelector('.mapsindoors-map');
+        const sheetContainer = document.querySelector('.react-modal-sheet-container');
+        if (sheetContainer) {
+            resolve(getReactModalSheetVisibleHeight(sheetContainer));
+            return;
+        }
 
-        // If there is no bottom sheet element (yet), setup a MutationObserver that listens for the appearance of it.
-        if (!bottomSheet) {
+        const observer = new MutationObserver(() => {
             const sheetContainer = document.querySelector('.react-modal-sheet-container');
             if (sheetContainer) {
+                observer.disconnect();
                 resolve(getReactModalSheetVisibleHeight(sheetContainer));
-                return;
             }
+        });
 
-            const observer = new MutationObserver(() => {
-                const bottomSheet = document.querySelector('.sheet--active');
-                if (bottomSheet) {
-                    observer.disconnect();
-                    resolve(mapContainer.offsetHeight - bottomSheet.offsetTop); // Subtract the top padding from the height of the map container element.
-                    return;
-                }
-                const sheetContainer = document.querySelector('.react-modal-sheet-container');
-                if (sheetContainer) {
-                    observer.disconnect();
-                    resolve(getReactModalSheetVisibleHeight(sheetContainer));
-                }
-            });
-
-            observer.observe(document, { attributes: false, childList: true, characterData: false, subtree: true });
-        } else {
-            resolve(mapContainer.offsetHeight - bottomSheet.offsetTop)
-        }
+        observer.observe(document, { attributes: false, childList: true, characterData: false, subtree: true });
     });
 }
 
