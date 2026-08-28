30c9da5831cf3be258d808a668efd293b73cea35
Mateusz <matb@mapspeople.com>
2026-08-28 10:05:09 +0200

    Update @mapsindoors/map-template to version 1.99.20 and bump web SDK to 4.59.4

--- packages/map-template/src/components/MapTemplate/MapTemplate.jsx
+++ packages/map-template/src/components/MapTemplate/MapTemplate.jsx
@@ -276,8 +276,8 @@ function MapTemplate({ apiKey, gmApiKey, mapboxAccessToken, venue, locationId, p
             const miSdkApiTag = document.createElement('script');
             miSdkApiTag.setAttribute('type', 'text/javascript');
             // Remember to update the root index.html with the same version / integrity
-            miSdkApiTag.setAttribute('src', 'https://app.mapsindoors.com/mapsindoors/js/sdk/4.59.3/mapsindoors-4.59.3.js.gz');
-            miSdkApiTag.setAttribute('integrity', 'sha384-86wq0HRXmYDpFynzv4xQwuFvBvrLhdkQnLaUgffO7ZbBk6RFX3S6pskbdKQTCTFG');
+            miSdkApiTag.setAttribute('src', 'https://app.mapsindoors.com/mapsindoors/js/sdk/4.59.4/mapsindoors-4.59.4.js.gz');
+            miSdkApiTag.setAttribute('integrity', 'sha384-Tzu1HA15zCxWXZLJdq+CXWHSy4utTjfvJaG/NyDaYPhIoD+JY8w5FxLoxjltI77M');
             miSdkApiTag.setAttribute('crossorigin', 'anonymous');
             document.body.appendChild(miSdkApiTag);
             miSdkApiTag.onload = () => {
